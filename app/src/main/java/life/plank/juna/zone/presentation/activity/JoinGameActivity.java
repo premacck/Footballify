6051975353cb8a98893785aade0f765519fd9f5f
plank-sobia <sobia@plank.life>
2017-10-06 19:14:57 +0530

    JUNA-924 #comment UI for points match game * UI added for football matches * reactive clicks added for each cell * Team logos added

--- app/src/main/java/life/plank/juna/zone/presentation/activity/JoinGameActivity.java
+++ app/src/main/java/life/plank/juna/zone/presentation/activity/JoinGameActivity.java
@@ -2,31 +2,20 @@ package life.plank.juna.zone.presentation.activity;
 
 import android.content.Intent;
 import android.graphics.Typeface;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
-
-import java.util.ArrayList;
-import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import life.plank.juna.zone.R;
-import life.plank.juna.zone.data.network.model.Arena;
-import life.plank.juna.zone.data.network.model.Round;
 import life.plank.juna.zone.util.CustomizeStatusBar;
 
 public class JoinGameActivity extends AppCompatActivity {
 
     @BindView(R.id.league_name)
     TextView leagueName;
-
-    @OnClick(R.id.button_join_game)
-    public void joinPointsGame() {
-        startActivity(new Intent(this, PointsMatchActivity.class));
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,5 +26,15 @@ public class JoinGameActivity extends AppCompatActivity {
 
         Typeface moderneSansFont = Typeface.createFromAsset(getAssets(), "font/san-frediano.ttf");
         leagueName.setTypeface(moderneSansFont);
+    }
+
+    @OnClick(R.id.button_join_game)
+    public void joinPointsGame() {
+        startActivity(new Intent(this, PointsMatchActivity.class));
+    }
+
+    @OnClick(R.id.home_icon)
+    public void exitPointsGame() {
+        startActivity(new Intent(this, GameLaunchActivity.class));
     }
 }
--- app/src/main/java/life/plank/juna/zone/presentation/adapter/PointsMatchAdapter.java
+++ app/src/main/java/life/plank/juna/zone/presentation/adapter/PointsMatchAdapter.java
@@ -1,9 +1,11 @@
 package life.plank.juna.zone.presentation.adapter;
 
+import android.graphics.Typeface;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -12,7 +14,9 @@ import java.util.List;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import life.plank.juna.zone.R;
+import life.plank.juna.zone.ZoneApplication;
 import life.plank.juna.zone.data.network.model.FootballMatch;
+import life.plank.juna.zone.util.TeamNameMap;
 
 /**
  * Created by plank-sobia on 10/5/2017.
@@ -21,16 +25,23 @@ import life.plank.juna.zone.data.network.model.FootballMatch;
 public class PointsMatchAdapter extends RecyclerView.Adapter<PointsMatchAdapter.ViewHolder> {
 
     List<FootballMatch> footballMatchList = new ArrayList<>();
+    Typeface moderneSansFont = Typeface.createFromAsset(ZoneApplication.getContext().getAssets(), "font/moderne_sans.ttf");
 
     public class ViewHolder extends RecyclerView.ViewHolder {
         @BindView(R.id.home_team_name)
         TextView homeTeamName;
         @BindView(R.id.visiting_team_name)
         TextView visitingTeamName;
+        @BindView(R.id.home_team_image)
+        ImageView homeTeamImage;
+        @BindView(R.id.visiting_team_image)
+        ImageView visitingTeamImage;
 
         public ViewHolder(View itemView) {
             super(itemView);
             ButterKnife.bind(this, itemView);
+            homeTeamName.setTypeface(moderneSansFont);
+            visitingTeamName.setTypeface(moderneSansFont);
         }
     }
 
@@ -38,6 +49,7 @@ public class PointsMatchAdapter extends RecyclerView.Adapter<PointsMatchAdapter.
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.match_points_rows, parent, false);
+        TeamNameMap.HashMaps(parent.getContext());
         return new ViewHolder(view);
     }
 
@@ -45,7 +57,9 @@ public class PointsMatchAdapter extends RecyclerView.Adapter<PointsMatchAdapter.
     public void onBindViewHolder(ViewHolder holder, int position) {
         final FootballMatch footballMatch = footballMatchList.get(position);
         holder.homeTeamName.setText(footballMatch.getHomeTeam().getName());
+        holder.homeTeamImage.setImageDrawable(TeamNameMap.getTeamNameMap().get(footballMatch.getHomeTeam().getName()));
         holder.visitingTeamName.setText(footballMatch.getVisitingTeam().getName());
+        holder.visitingTeamImage.setImageDrawable(TeamNameMap.getTeamNameMap().get(footballMatch.getVisitingTeam().getName()));
     }
 
 
