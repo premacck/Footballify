589ce050bb68e2e1f4e1cafaced672b1a240976e
plank-dhamini <dhamini@plank.life>
2017-11-18 14:37:12 +0530

    JUNA-1046 #comment enabled push notification service * Enabled push notification service * Added dependencies for push notification service * Added google_services.json

---
+++ app/src/main/java/life/plank/juna/zone/pushNotification/NotificationInstanceIDService.java
@@ -0,0 +1,24 @@
+package life.plank.juna.zone.pushNotification;
+
+/**
+ * Created by plank-dhamini on 18/11/17.
+ */
+
+import android.content.Intent;
+import android.util.Log;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+
+public class NotificationInstanceIDService extends FirebaseInstanceIdService {
+
+    private static final String TAG = "NotificationInstanceIDService";
+
+    @Override
+    public void onTokenRefresh() {
+
+        Log.d(TAG, "Refreshing GCM Registration Token");
+
+        Intent intent = new Intent(this, RegistrationIntentService.class);
+        startService(intent);
+    }
+};
