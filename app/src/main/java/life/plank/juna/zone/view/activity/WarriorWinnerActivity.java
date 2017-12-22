1b6d83f307ed0af6be504950b62d9904a7cc9076
plank-dhamini <dhamini@plank.life>
2017-12-22 20:37:17 +0530

    JUNA-1129 #comment removed unnecessary import statements

--- app/src/main/java/life/plank/juna/zone/view/activity/WarriorWinnerActivity.java
+++ app/src/main/java/life/plank/juna/zone/view/activity/WarriorWinnerActivity.java
@@ -1,11 +1,8 @@
 package life.plank.juna.zone.view.activity;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
 
-import butterknife.OnClick;
 import life.plank.juna.zone.R;
 
 /**
@@ -19,5 +16,5 @@ public class WarriorWinnerActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_warrior_winner);
     }
-    
+
 }
