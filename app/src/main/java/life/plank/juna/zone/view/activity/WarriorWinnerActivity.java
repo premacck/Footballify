753b197a8c16a2a1f1b432a5db7ca8b511fa3859
plank-dhamini <dhamini@plank.life>
2018-01-06 18:53:38 +0530

    JUNA-1181 #comment club warrior winner view * Navigating to the club warrior winner view on completion of all 20 rounds

--- app/src/main/java/life/plank/juna/zone/view/activity/WarriorWinnerActivity.java
+++ app/src/main/java/life/plank/juna/zone/view/activity/WarriorWinnerActivity.java
@@ -1,8 +1,13 @@
 package life.plank.juna.zone.view.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import life.plank.juna.zone.R;
 
 /**
@@ -11,10 +16,26 @@ import life.plank.juna.zone.R;
 
 public class WarriorWinnerActivity extends AppCompatActivity {
 
+    @BindView(R.id.score_text)
+    TextView score;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_warrior_winner);
+
+        ButterKnife.bind(this);
+        score.setText(getIntent().getStringExtra("score"));
     }
 
+    @OnClick(R.id.home_icon)
+    public void onClick() {
+        Intent intent = new Intent(this, ClubListViewActivity.class);
+        startActivity(intent);
+    }
+
+    @Override
+    public void onBackPressed() {
+
+    }
 }
