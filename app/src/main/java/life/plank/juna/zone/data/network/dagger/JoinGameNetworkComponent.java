0761da2656aeac333df85c3f7bd491eaa1ad4714
Arfaa Nisar Shariff <arfaa@plank.life>
2018-01-10 19:47:36 +0530

    JUNA-1184 #comment multiplayer game * Setting arena on clicking joinGame button

---
+++ app/src/main/java/life/plank/juna/zone/data/network/dagger/JoinGameNetworkComponent.java
@@ -0,0 +1,17 @@
+package life.plank.juna.zone.data.network.dagger;
+
+import javax.inject.Singleton;
+
+import dagger.Component;
+import life.plank.juna.zone.data.network.module.RestServiceModule;
+import life.plank.juna.zone.view.activity.JoinGameActivity;
+
+/**
+ * Created by plank-arfaa on 10/01/18.
+ */
+
+@Singleton
+@Component(modules = RestServiceModule.class)
+public interface JoinGameNetworkComponent {
+    void inject(JoinGameActivity joinGameActivity);
+}
