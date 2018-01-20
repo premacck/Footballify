4f7f303c00e86ba893d50ef5908a5f59cc2ed802
Arfaa Nisar Shariff <arfaa@plank.life>
2018-01-20 10:27:06 +0530

    JUNA-1191 #comment * Added FootballFeedAdapter to display dummy data in the grid * Added background for page view * Added UI for grid

--- app/src/main/java/life/plank/juna/zone/view/fragment/SwipePageFragment.java
+++ app/src/main/java/life/plank/juna/zone/view/fragment/SwipePageFragment.java
@@ -2,20 +2,23 @@ package life.plank.juna.zone.view.fragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import life.plank.juna.zone.R;
+import life.plank.juna.zone.view.adapter.FootballFeedAdapter;
 
 /**
  * Created by plank-arfaa on 19/01/18.
  */
 
-public class SwipePageFragment extends Fragment {
+public class SwipePageFragment extends Fragment implements FootballFeedAdapter.ItemClickListener {
 
     private int pageNumber;
+    FootballFeedAdapter footballFeedAdapter;
 
     public static SwipePageFragment create(int pageNumber) {
         SwipePageFragment fragment = new SwipePageFragment();
@@ -42,7 +45,17 @@ public class SwipePageFragment extends Fragment {
 
         // Set the title view to show the page number.
         //TODO: Will be replaced with the feed grid
-        ((TextView) rootView.findViewById(R.id.dummyText)).setText("Hello");
+        //((TextView) rootView.findViewById(R.id.dummyText)).setText("Hello");
+
+        String[] data = {"1", "2", "3", "4"};
+
+        // set up the RecyclerView
+        RecyclerView recyclerView = (RecyclerView) rootView.findViewById(R.id.football_feed_recycler_view);
+        int numberOfColumns = 2;
+        recyclerView.setLayoutManager(new GridLayoutManager(getContext(), numberOfColumns));
+        footballFeedAdapter = new FootballFeedAdapter(getContext(), data);
+        footballFeedAdapter.setClickListener(this);
+        recyclerView.setAdapter(footballFeedAdapter);
 
         return rootView;
     }
@@ -51,4 +64,10 @@ public class SwipePageFragment extends Fragment {
         return pageNumber;
     }
 
+    @Override
+    public void onItemClick(View view, int position) {
+
+
+
+    }
 }
