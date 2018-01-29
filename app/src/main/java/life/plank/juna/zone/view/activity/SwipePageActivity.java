46bdadb9a40832362c07fbe61e6ca880039341f6
Arfaa Nisar Shariff <arfaa@plank.life>
2018-01-29 10:19:09 +0530

    JUNA-1208 #comment * Removed the View pager and retained only a recycler view . * Commented code to get horizontally swipable grids * Commented code will be deleted after the functionality is complete

--- app/src/main/java/life/plank/juna/zone/view/activity/SwipePageActivity.java
+++ app/src/main/java/life/plank/juna/zone/view/activity/SwipePageActivity.java
@@ -1,9 +1,8 @@
 package life.plank.juna.zone.view.activity;
 
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
-import android.support.v4.view.PagerAdapter;
-import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.widget.ArrayAdapter;
@@ -11,37 +10,39 @@ import android.widget.Spinner;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
-import butterknife.OnPageChange;
 import life.plank.juna.zone.R;
+import life.plank.juna.zone.view.adapter.FootballFeedAdapter;
 import life.plank.juna.zone.view.adapter.HorizontalFootballFeedAdapter;
-import life.plank.juna.zone.view.adapter.SwipePageAdapter;
 
 
 /**
  * Created by plank-arfaa on 19/01/18.
  */
 
-public class SwipePageActivity extends FragmentActivity {
+public class SwipePageActivity extends AppCompatActivity {
 
 
-    @BindView(R.id.pager)
-    ViewPager pager;
+//    @BindView(R.id.pager)
+//    ViewPager pager;
     @BindView(R.id.calendar_spinner)
     Spinner calendarSpinner;
     @BindView(R.id.football_feed_horizontal_view)
     RecyclerView horizontalRecyclerView;
+    @BindView(R.id.football_feed_recycler_view)
+    RecyclerView feedRecyclerView;
 
 
-    private PagerAdapter pagerAdapter;
+//    private PagerAdapter pagerAdapter;
     HorizontalFootballFeedAdapter horizontalfootballFeedAdapter;
+    FootballFeedAdapter footballFeedAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_swipe_page);
         ButterKnife.bind(this);
-        pagerAdapter = new SwipePageAdapter(getSupportFragmentManager());
-        pager.setAdapter(pagerAdapter);
+//        pagerAdapter = new SwipePageAdapter(getSupportFragmentManager());
+//        pager.setAdapter(pagerAdapter);
 
         String[] horizontalData = {"6S: MARK F",
                 "14S: ROBERT",
@@ -56,6 +57,20 @@ public class SwipePageActivity extends FragmentActivity {
         horizontalfootballFeedAdapter = new HorizontalFootballFeedAdapter(this, horizontalData);
         horizontalRecyclerView.setAdapter(horizontalfootballFeedAdapter);
 
+        //Feed recycler view
+
+        String[] data = {"Reece Burke celebrates after breaking the deadlock in extra time at London Stadium",
+                "Utd to subsidised fans in Sevilla row. $89 to visit Old Trafford for spanish supporters",
+                "Charlie Nicholas : FA Cup third round replay predictions",
+                "Brighton vs Chelsea"};
+
+
+        int numberOfRows = 2;
+//        feedRecyclerView.setLayoutManager(new GridLayoutManager(this, numberOfColumns));
+        feedRecyclerView.setLayoutManager(new GridLayoutManager(this,numberOfRows, GridLayoutManager.HORIZONTAL,false));
+
+        footballFeedAdapter = new FootballFeedAdapter(this, data);
+        feedRecyclerView.setAdapter(footballFeedAdapter);
 
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
                 R.array.calendar_array, R.layout.custom_calendar_spinner);
@@ -63,10 +78,10 @@ public class SwipePageActivity extends FragmentActivity {
         calendarSpinner.setAdapter(adapter);
     }
 
-    @OnPageChange(R.id.pager)
-    public void onPageChanged() {
-        invalidateOptionsMenu();
-    }
+//    @OnPageChange(R.id.pager)
+//    public void onPageChanged() {
+//        invalidateOptionsMenu();
+//    }
 
 }
 
