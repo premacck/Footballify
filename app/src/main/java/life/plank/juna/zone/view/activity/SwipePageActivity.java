df3705a9e216918739db4de31561fda229ef2908
Niraj Kulal <niraj@plank.life>
2018-01-29 13:34:06 +0530

    JUNA-1209 #commentFixed issue with horizontal size so that grid count will be same in all resolutions.

--- app/src/main/java/life/plank/juna/zone/view/activity/SwipePageActivity.java
+++ app/src/main/java/life/plank/juna/zone/view/activity/SwipePageActivity.java
@@ -5,12 +5,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SnapHelper;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.util.TypedValue;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.Spinner;
-
+import java.util.ArrayList;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import life.plank.juna.zone.R;
+import life.plank.juna.zone.util.helper.StartSnapHelper;
 import life.plank.juna.zone.view.adapter.FootballFeedAdapter;
 import life.plank.juna.zone.view.adapter.HorizontalFootballFeedAdapter;
 
@@ -27,6 +33,8 @@ public class SwipePageActivity extends AppCompatActivity {
     RecyclerView horizontalRecyclerView;
     @BindView(R.id.football_feed_recycler_view)
     RecyclerView feedRecyclerView;
+    @BindView(R.id.zone_logo)
+    ImageView zoneLogo;
 
     HorizontalFootballFeedAdapter horizontalfootballFeedAdapter;
     FootballFeedAdapter footballFeedAdapter;
@@ -36,34 +44,46 @@ public class SwipePageActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_swipe_page);
         ButterKnife.bind(this);
+        ArrayList<String> horizontalData = new ArrayList<>();
+        horizontalData.add("6S: MARK F");
+        horizontalData.add("23S: SUE M");
+        horizontalData.add("27S: GRAHAM");
+        horizontalData.add("6S: MARK F");
 
-        String[] horizontalData = {"6S: MARK F",
-                "14S: ROBERT",
-                "23S: SUE M",
-                "27S: GRAHAM",
-                "6S: MARK F",
-                "14S: ROBERT",
-                "23S: SUE M",
-                "27S: GRAHAM"};
+        // Calculate ActionBar height
+        int actionBarHeight = 0;
+        TypedValue typedValue = new TypedValue();
+        if (getTheme().resolveAttribute(android.R.attr.actionBarSize, typedValue, true)) {
+            actionBarHeight = TypedValue.complexToDimensionPixelSize(typedValue.data, getResources().getDisplayMetrics());
+        }
         //Setup the horizontal recycler view
         horizontalRecyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
         horizontalfootballFeedAdapter = new HorizontalFootballFeedAdapter(this, horizontalData);
         horizontalRecyclerView.setAdapter(horizontalfootballFeedAdapter);
 
         //Feed recycler view
-
         String[] data = {"Reece Burke celebrates after breaking the deadlock in extra time at London Stadium",
+                "Utd to subsidised fans in Sevilla row. $89 to visit Old Trafford for spanish supporters",
+                "Charlie Nicholas : FA Cup third round replay predictions",
+                "Brighton vs Chelsea",
+                "Reece Burke celebrates after breaking the deadlock in extra time at London Stadium",
                 "Utd to subsidised fans in Sevilla row. $89 to visit Old Trafford for spanish supporters",
                 "Charlie Nicholas : FA Cup third round replay predictions",
                 "Brighton vs Chelsea"};
 
 
         int numberOfRows = 2;
-        feedRecyclerView.setLayoutManager(new GridLayoutManager(this,numberOfRows, GridLayoutManager.HORIZONTAL,false));
-
-        footballFeedAdapter = new FootballFeedAdapter(this, data);
+//        feedRecyclerView.setLayoutManager(new GridLayoutManager(this, numberOfColumns));
+        feedRecyclerView.setLayoutManager(new GridLayoutManager(this, numberOfRows, GridLayoutManager.HORIZONTAL, false));
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        int height = displayMetrics.heightPixels;
+        int width = displayMetrics.widthPixels;
+        footballFeedAdapter = new FootballFeedAdapter(this, data, height, width);
         feedRecyclerView.setAdapter(footballFeedAdapter);
-
+        // TODO: 29-01-2018 Change based on performance
+        SnapHelper snapHelper = new StartSnapHelper();
+        snapHelper.attachToRecyclerView(feedRecyclerView);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
                 R.array.calendar_array, R.layout.custom_calendar_spinner);
         adapter.setDropDownViewResource(R.layout.calendar_spinner_dropdown_item);
