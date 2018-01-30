116040c4c9ea2df35a94ac881deb3005560acc38
Niraj Kulal <niraj@plank.life>
2018-01-30 16:44:30 +0530

    JUNA-1209 #comment * Added add more button in the recycler view added more data onClick of this button..

--- app/src/main/java/life/plank/juna/zone/view/adapter/HorizontalFootballFeedAdapter.java
+++ app/src/main/java/life/plank/juna/zone/view/adapter/HorizontalFootballFeedAdapter.java
@@ -6,6 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -18,29 +19,60 @@ import life.plank.juna.zone.R;
  * Created by plank-arfaa on 22/01/18.
  */
 
-public class HorizontalFootballFeedAdapter extends RecyclerView.Adapter<HorizontalFootballFeedAdapter.ViewHolder> {
+public class HorizontalFootballFeedAdapter extends RecyclerView.Adapter<HorizontalFootballFeedAdapter.FootballFeedViewHolder> {
 
-    int image = R.drawable.ic_dummy_user;
+    private AddMoreClickListeners clickListeners;
     //TODO:Will be replaced with data from the backend
-    private ArrayList<String> data = new ArrayList();
+    private ArrayList data = new ArrayList();
     private LayoutInflater mInflater;
+    private int screenWidth;
+    private Context context;
+    private int footballBanterViewMargin;
+    private static final int NORMAL_VIEW = 0;
+    private static final int ADD_MORE_VIEW = 1;
 
-    public HorizontalFootballFeedAdapter(Context context, ArrayList data) {
+    public HorizontalFootballFeedAdapter(Context context, ArrayList data, int width) {
         this.mInflater = LayoutInflater.from(context);
+        this.context = context;
         this.data = data;
+        screenWidth = width;
+        //Result will be in pixels
+        footballBanterViewMargin = (int) context.getResources().getDimension(R.dimen.football_banter_view_margin);
+        this.clickListeners = (AddMoreClickListeners) context;
     }
 
     @Override
-    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view = mInflater.inflate(R.layout.horizontal_football_feed_row, parent, false);
-        return new ViewHolder(view);
+    public FootballFeedViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view;
+        if (viewType == NORMAL_VIEW) {
+            view = mInflater.inflate(R.layout.horizontal_football_feed_row, parent, false);
+        }else{
+            view = mInflater.inflate(R.layout.add_more_row, parent, false);
+        }
+        return new FootballFeedViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(ViewHolder holder, int position) {
-        String text = data.get(position);
-        holder.horizontalNewsFeedLabel.setText(text);
-        holder.horizontalNewsFeedImage.setImageResource(image);
+    public void onBindViewHolder(FootballFeedViewHolder holder, int position) {
+        String text = (String) data.get(position);
+        if (position != data.size()) {
+            holder.horizontalNewsFeedLabel.setText(text);
+        }
+        //Padding of single cell. footballBanterViewMargin*2 (2 sides of cell)
+        // TO get equal four cell (screenWidth / 4)
+        // TODO: 30-01-2018 deice number based on width
+        holder.horizontalNewsCardLayout.getLayoutParams().width = (screenWidth / 4) - (footballBanterViewMargin * 2);
+        holder.horizontalNewsCardLayout.setOnClickListener(view -> clickListeners.addMore());
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        // TODO: 30-01-2018 change this based on server response 
+        if (data.get(position).equals("addMore")) {
+            return ADD_MORE_VIEW;
+        } else {
+            return NORMAL_VIEW;
+        }
     }
 
     @Override
@@ -48,13 +80,18 @@ public class HorizontalFootballFeedAdapter extends RecyclerView.Adapter<Horizont
         return data.size();
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder {
+    public interface AddMoreClickListeners {
+        void addMore();
+    }
+
+    class FootballFeedViewHolder extends RecyclerView.ViewHolder {
         @BindView(R.id.horizontal_news_label)
         TextView horizontalNewsFeedLabel;
         @BindView(R.id.horizontal_news_image)
         ImageView horizontalNewsFeedImage;
-
-        ViewHolder(View itemView) {
+        @BindView(R.id.card_content)
+        RelativeLayout horizontalNewsCardLayout;
+        FootballFeedViewHolder(View itemView) {
             super(itemView);
             ButterKnife.bind(this, itemView);
         }
