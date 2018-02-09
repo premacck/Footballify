7c80059efc5b4fca35fb96e9a4573c3f4bc58be3
Hasan Shaikh <hasan@plank.life>
2018-02-09 16:08:06 +0530

    JUNA-1259 #comment animation smoothness issue fixed

--- app/src/main/java/life/plank/juna/zone/view/activity/SwipePageActivity.java
+++ app/src/main/java/life/plank/juna/zone/view/activity/SwipePageActivity.java
@@ -235,7 +235,7 @@ public class SwipePageActivity extends AppCompatActivity implements HorizontalFo
         fragmentContainerFrameLayout.removeAllViews();
         getSupportFragmentManager()
                 .beginTransaction()
-                .setCustomAnimations(R.anim.slide_in, R.anim.slide_out, R.anim.slide_in, R.anim.slide_out)
+                .setCustomAnimations(R.anim.slide_in, R.anim.slide_out)
                 .replace(R.id.fragmentContainerFrameLayout, new LiveZoneFragment())
                 .commit();
 
