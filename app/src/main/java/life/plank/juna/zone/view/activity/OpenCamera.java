de432fec0b9fd1e57f613bd034ea72c1ae60c58b
PLANK\plank-sharath <sharath@plank.life>
2018-02-15 18:55:08 +0530

    JUNA-1283 # coment * open camera

--- app/src/main/java/life/plank/juna/zone/view/activity/OpenCamera.java
+++ app/src/main/java/life/plank/juna/zone/view/activity/OpenCamera.java
@@ -4,12 +4,12 @@ import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -17,36 +17,43 @@ import android.widget.Toast;
 import java.io.File;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.Random;
 
 import life.plank.juna.zone.R;
 
 public class OpenCamera extends AppCompatActivity implements View.OnClickListener {
-    Button takepicture,gallery_Button;
+    Button takepicture, gallery_Button;
     Uri file;
+    
+    private static File getOutputMediaFile() {
 
+        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_DCIM), "Pictures");
+        if (!mediaStorageDir.exists()) {
+            if (!mediaStorageDir.mkdirs()) {
+                return null;
+            }
+        }
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        return new File(mediaStorageDir.getPath() + File.separator +
+                "IMG_" + timeStamp + ".jpg");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_open_camera);
-         takepicture =(Button)findViewById(R.id.button_image);
-         gallery_Button=(Button)findViewById(R.id.gallery_Button);
-         takepicture.setOnClickListener(this);
-         gallery_Button.setOnClickListener(this);
+        takepicture = (Button) findViewById(R.id.button_image);
+        gallery_Button = (Button) findViewById(R.id.gallery_Button);
+        takepicture.setOnClickListener(this);
+        gallery_Button.setOnClickListener(this);
 
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED)
-        {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
             takepicture.setEnabled(false);
-            ActivityCompat.requestPermissions(this, new String[] { Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE,Manifest.permission.READ_EXTERNAL_STORAGE }, 0);
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE}, 0);
         }
 
 
-
-
     }
-
-
 
     @Override
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
@@ -58,30 +65,18 @@ public class OpenCamera extends AppCompatActivity implements View.OnClickListene
         }
     }
 
-
-
-
-
-
     public void takePicture(View view) {
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         file = Uri.fromFile(getOutputMediaFile());
         intent.putExtra(MediaStore.EXTRA_OUTPUT, file);
-
         startActivityForResult(intent, 100);
     }
 
     @Override
-    public void onClick(View view)
-    {
-
-
-        if(view==takepicture)
-        {
-        takePicture(view);
-        }
-    else
-        {
+    public void onClick(View view) {
+        if (view == takepicture) {
+            takePicture(view);
+        } else {
             Intent intent = new Intent(Intent.ACTION_PICK,
                     android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
             intent.setAction(android.content.Intent.ACTION_VIEW);
@@ -98,36 +93,10 @@ public class OpenCamera extends AppCompatActivity implements View.OnClickListene
             if (resultCode == RESULT_OK) {
 
 
-
-
-
-                Toast.makeText(this,"Image Saved to Gallery",Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, "Image Saved to Gallery", Toast.LENGTH_SHORT).show();
             }
         }
     }
-
-    private static File getOutputMediaFile(){
-
-
-
-
-
-
-        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
-                Environment.DIRECTORY_DCIM), "Pictures");
-
-        if (!mediaStorageDir.exists()){
-            if (!mediaStorageDir.mkdirs()){
-                return null;
-            }
-        }
-
-        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-        return new File(mediaStorageDir.getPath() + File.separator +
-                "IMG_"+ timeStamp + ".jpg");
-    }
-
-
 
 
 }
