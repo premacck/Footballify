f291471d0c2013529417c17ecfb7829d87867efc
PLANK\plank-sharath <sharath@plank.life>
2018-02-16 11:07:46 +0530

    JUNA-1283 #comment * open camera

--- app/src/main/java/life/plank/juna/zone/view/activity/OpenCamera.java
+++ app/src/main/java/life/plank/juna/zone/view/activity/OpenCamera.java
@@ -13,27 +13,38 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.File;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import life.plank.juna.zone.R;
 
-public class OpenCamera extends AppCompatActivity implements View.OnClickListener {
-    Button takepicture, gallery_Button;
+public class OpenCamera extends AppCompatActivity {
+
+    @BindView(R.id.button_image)
+    Button takepicture;
+
+    @BindView(R.id.gallery_Button)
+    Button gallery_Button;
+
+
     Uri file;
     static Context context;
     public static File getOutputMediaFile() {
 
         File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
                 Environment.DIRECTORY_DCIM),context.getString(R.string.Child_parameter) );
-        if (!mediaStorageDir.exists()) {
-            if (!mediaStorageDir.mkdirs()) {
+        if (!mediaStorageDir.exists() && !mediaStorageDir.mkdirs())
+             {
                 return null;
             }
-        }
+
         String timeStamp = new SimpleDateFormat(context.getString(R.string.date_format)).format(new Date());
         return new File(mediaStorageDir.getPath() + File.separator +
                 context.getString(R.string.image_name) + timeStamp +context.getString (R.string.extention));
@@ -43,10 +54,8 @@ public class OpenCamera extends AppCompatActivity implements View.OnClickListene
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_open_camera);
-        takepicture = (Button) findViewById(R.id.button_image);
-        gallery_Button = (Button) findViewById(R.id.gallery_Button);
-        takepicture.setOnClickListener(this);
-        gallery_Button.setOnClickListener(this);
+        ButterKnife.bind(this);
+      
 
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
             takepicture.setEnabled(false);
@@ -73,20 +82,31 @@ public class OpenCamera extends AppCompatActivity implements View.OnClickListene
         startActivityForResult(intent, 100);
     }
 
-    @Override
-    public void onClick(View view) {
-        if (view == takepicture) {
-            takePicture(view);
-        } else {
-            Intent intent = new Intent(Intent.ACTION_PICK,
-                    android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-            intent.setAction(android.content.Intent.ACTION_VIEW);
-            intent.setType(getString(R.string.setimagetype));
-            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            startActivity(intent);
 
+
+
+    @OnClick({R.id.button_image, R.id.gallery_Button})
+    public void onViewClicked(View view) {
+
+        switch (view.getId()) {
+            case R.id.button_image:
+                takePicture(view);
+                break;
+            case R.id.gallery_Button:
+                Intent intent = new Intent(Intent.ACTION_PICK,
+                        android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                intent.setAction(android.content.Intent.ACTION_VIEW);
+                intent.setType(getString(R.string.setimagetype));
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+                break;
         }
+
+
     }
+
+
+
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
