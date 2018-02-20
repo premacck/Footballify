7590222761c62b57e243c352931606dccbbdd973
Niraj Kulal <niraj@plank.life>
2018-02-20 12:51:35 +0530

    JUNA-1307 #comment * Added missed import

--- app/src/main/java/life/plank/juna/zone/view/fragment/LiveZoneFragment.java
+++ app/src/main/java/life/plank/juna/zone/view/fragment/LiveZoneFragment.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
