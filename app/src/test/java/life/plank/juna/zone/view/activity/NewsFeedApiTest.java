c5bb0a8b2a36f566629ae7e01683465fdae3e745
PLANK\plank-sharath <sharath@plank.life>
2018-02-23 11:56:52 +0530

    JUNA-1304 # comments * getting the header key and harcode it

---
+++ app/src/test/java/life/plank/juna/zone/view/activity/NewsFeedApiTest.java
@@ -0,0 +1,40 @@
+package life.plank.juna.zone.view.activity;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+
+import static net.bytebuddy.matcher.ElementMatchers.is;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Created by plank-sharath on 2/23/2018.
+ */
+
+public class NewsFeedApiTest {
+
+    @InjectMocks
+    SwipePageActivity swipePageActivity;
+
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void CheckNewsFeedAPi()
+    {
+
+     String check=swipePageActivity.updateToken("{\"token\":\"+RID:S6gVAN+HdgBLAAAAAAAAAA==#RT:1#TRC:20#RTD:vjAyMDE4LTAyLTE4VDE0OjMwOjAwWg==\",\"range\":{\"min\":\"\",\"max\":\"FF\"}}"
+                ,"RT:(\\d*)","TRC:(\\\\d*)","RT:2","TRC:40");
+
+        assertEquals(check.isEmpty(),(false));
+    }
+
+   
+
+
+}
