e2f81e51ee464a21f118e24b2314c1065aacb8a7
PLANK\plank-prachi <prachi@plank.life>
2018-02-24 14:16:18 +0530

    JUNA-1302 #comment  updated AdapterTest Class

--- app/src/test/java/life/plank/juna/zone/domain/service/FootballFeedDetailsAdapterTest.java
+++ app/src/test/java/life/plank/juna/zone/domain/service/FootballFeedDetailsAdapterTest.java
@@ -1,13 +1,18 @@
 package life.plank.juna.zone.domain.service;
 
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import java.util.List;
+
+import life.plank.juna.zone.data.network.model.FootballFeed;
 import life.plank.juna.zone.view.adapter.FootballFeedDetailAdapter;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -16,26 +21,49 @@ import static org.mockito.Mockito.when;
  */
 
 public class FootballFeedDetailsAdapterTest {
+    @Mock
+    List<FootballFeed> footballFeedsList;
+    private FootballFeed footballFeed;
     @InjectMocks
     private FootballFeedDetailAdapter footballFeedDetailAdapter;
 
     @Before
     public void setUp() {
         MockitoAnnotations.initMocks(this);
+        footballFeed = new FootballFeed();
         footballFeedDetailAdapter = mock(FootballFeedDetailAdapter.class);
+
     }
 
-    //if data is coming with null value
+    @Test
+    public void isFeedDataNotEmpty() {
+        isDataNotEmpty();
+        assertThat(footballFeedsList.isEmpty(), is(false));
+    }
+
+    @Test
+    public void isFeedDataEmpty() {
+        isEmptyData();
+        for (int i = 0; i <= footballFeedsList.size() - 1; i++)
+            assertThat(footballFeedsList.get(i).getSummary().isEmpty(), is(true));
+    }
+
+    /* if list is null */
     @Test
     public void addItemsToHorizontalViewListIsNull() {
-        when(footballFeedDetailAdapter.getItemCount() == 0).thenThrow(NullPointerException.class);
-
+        when(footballFeedDetailAdapter.getItemCount() < 0).thenThrow(NullPointerException.class);
     }
 
-    //if data is coming with  value
-    @Test
-    public void addItemsToHorizontalViewListIs() {
-        int check = footballFeedDetailAdapter.getItemCount();
-        Assert.assertEquals(check > 0, true);
+    //model class with empty data
+    public void isEmptyData() {
+        footballFeed.setSummary("");
+        footballFeed.setTitle("Southampton");
     }
+
+    //added data in model class
+    public void isDataNotEmpty() {
+        footballFeed.setSummary("Man United");
+        footballFeed.setTitle("Southampton");
+    }
+
 }
