05975f1143b6c16affeff2b371268411e283e954
Niraj Kulal <niraj@plank.life>
2018-02-26 10:23:48 +0530

    JUNA-1309 #comment Added test cases

--- app/src/test/java/life/plank/juna/zone/view/activity/ScrubberActivityTest.java
+++ app/src/test/java/life/plank/juna/zone/view/activity/ScrubberActivityTest.java
@@ -1,19 +1,24 @@
 package life.plank.juna.zone.view.activity;
 
+import android.content.Context;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 import life.plank.juna.zone.data.network.model.ScrubberViewData;
+import life.plank.juna.zone.interfaces.ScrubberPointerUpdate;
 import life.plank.juna.zone.util.ScrubberConstants;
 import life.plank.juna.zone.view.adapter.ScrubberViewAdapter;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.mock;
 
 /**
  * Created by plank-niraj on 23-02-2018.
@@ -21,62 +26,67 @@ import static org.junit.Assert.assertThat;
 
 public class ScrubberActivityTest {
 
-    // To check the event trigger.
-    private static final int EVENT = 7;
     @Mock
     HashMap<Integer, ScrubberViewData> scrubberViewDataHolder;
     @Mock
+    Context context;
     LiveZoneActivity liveZoneActivity;
     @Mock
-    private ScrubberViewAdapter scrubberViewAdapter;
+    ScrubberPointerUpdate scrubberPointerUpdate;
+    ArrayList<Integer> data;
+    ScrubberViewAdapter scrubberViewAdapter;
 
     @Before
     public void setUp() {
         MockitoAnnotations.initMocks(this);
         scrubberViewDataHolder = new HashMap<>();
+        liveZoneActivity = mock(LiveZoneActivity.class);
+        data = new ArrayList<>();
+        scrubberViewAdapter = Mockito.spy(new ScrubberViewAdapter(context,
+                data, scrubberViewDataHolder, scrubberPointerUpdate, 1));
         ScrubberConstants.getHighLightsMatchOne(scrubberViewDataHolder);
     }
 
-    /**
-     * Test to check dataSet update.
-     */
     @Test
-    public void testDataSet() {
+    public void testDataSetForEmpty() {
         scrubberViewAdapter.addHardCodedData();
         assertThat(scrubberViewDataHolder.isEmpty(), is(false));
     }
 
-    /**
-     * Test to check the function call(onNewEvent).
-     */
     @Test
-    public void onNewEventTest() {
-        if (scrubberViewDataHolder.containsKey(EVENT) && scrubberViewDataHolder.get(EVENT).isTriggerEvents()) {
-            liveZoneActivity.onNewEvent(scrubberViewDataHolder.get(EVENT));
-        }
-        Mockito.verify(liveZoneActivity).onNewEvent(scrubberViewDataHolder.get(EVENT));
+    public void testAdapterDataUpdated() {
+        ArrayList<Integer> data = new ArrayList<>();
+        ScrubberViewAdapter scrubberViewAdapter = new ScrubberViewAdapter(context,
+                data, scrubberViewDataHolder, scrubberPointerUpdate, 1);
+        data.add(1);
+        assertThat(scrubberViewAdapter.getItemCount(), is(1));
     }
 
-    /**
-     * Test to check updateRecentEvents function call
-     */
     @Test
-    public void eventUpdateTest() {
-        if (scrubberViewDataHolder.containsKey(EVENT) && scrubberViewDataHolder.get(EVENT).isTriggerEvents()) {
-            liveZoneActivity.updateRecentEvents(EVENT);
-        }
-        Mockito.verify(liveZoneActivity).updateRecentEvents(EVENT);
+    public void testAdapterDataForProgress() {
+        data.clear();
+        data.add(ScrubberConstants.SCRUBBER_VIEW_PROGRESS);
+        assertThat(scrubberViewAdapter.getItemViewType(0), is(ScrubberConstants.SCRUBBER_VIEW_PROGRESS));
     }
 
-    /**
-     * Test to check displayTooltip function call
-     */
     @Test
-    public void toolTipDisplayTest() {
-        if (scrubberViewDataHolder.containsKey(EVENT) &&
-                scrubberViewDataHolder.get(EVENT).isTriggerEvents()) {
-            scrubberViewAdapter.displayTooltip(null, scrubberViewDataHolder.get(EVENT).getMessage());
-        }
-        Mockito.verify(scrubberViewAdapter).displayTooltip(null, scrubberViewDataHolder.get(EVENT).getMessage());
+    public void testAdapterDataForGoal() {
+        data.clear();
+        data.add(ScrubberConstants.SCRUBBER_VIEW_GOAL);
+        assertThat(scrubberViewAdapter.getItemViewType(0), is(ScrubberConstants.SCRUBBER_VIEW_GOAL));
+    }
+
+    @Test
+    public void testAdapterDataForHalfTime() {
+        data.clear();
+        data.add(ScrubberConstants.SCRUBBER_VIEW_GOAL);
+        assertThat(scrubberViewAdapter.getItemViewType(0), is(ScrubberConstants.SCRUBBER_VIEW_GOAL));
+    }
+
+    @Test
+    public void testAdapterDataForPointer() {
+        data.clear();
+        data.add(ScrubberConstants.SCRUBBER_VIEW_CURSOR);
+        assertThat(scrubberViewAdapter.getItemViewType(0), is(ScrubberConstants.SCRUBBER_VIEW_CURSOR));
     }
 }
