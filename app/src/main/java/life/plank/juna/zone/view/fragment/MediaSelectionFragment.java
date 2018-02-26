d0e3550a146a2546199838fd63a4d9739f05513e
PLANK\plank-prachi <prachi@plank.life>
2018-02-26 18:44:22 +0530

    JUNA-1323 #comment adapter xml added

--- app/src/main/java/life/plank/juna/zone/view/fragment/MediaSelectionFragment.java
+++ app/src/main/java/life/plank/juna/zone/view/fragment/MediaSelectionFragment.java
@@ -27,6 +27,7 @@ public class MediaSelectionFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        populateStandingRecyclerView();
         return inflater.inflate(R.layout.fragment_media_selection, container, false);
     }
 
