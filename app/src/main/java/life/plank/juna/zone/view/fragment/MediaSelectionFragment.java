b9738b25efbf6ef3b829c341acc7c1f3a280f358
PLANK\plank-prachi <prachi@plank.life>
2018-02-26 19:55:42 +0530

    JUNA-1323 #comment media selection click on text done

--- app/src/main/java/life/plank/juna/zone/view/fragment/MediaSelectionFragment.java
+++ app/src/main/java/life/plank/juna/zone/view/fragment/MediaSelectionFragment.java
@@ -12,12 +12,15 @@ import android.widget.TextView;
 import butterknife.BindView;
 import butterknife.OnClick;
 import life.plank.juna.zone.R;
-import life.plank.juna.zone.util.CustomLinearLayoutManager;
 import life.plank.juna.zone.view.adapter.MediaSelectionAdapter;
 
 public class MediaSelectionFragment extends Fragment {
     @BindView(R.id.photos_text_view)
     TextView photosTextView;
+    @BindView(R.id.stickers_text_view)
+    TextView stickersTextView;
+    @BindView(R.id.gifs_text_view)
+    TextView gifsTextView;
     @BindView(R.id.media_container_recycler_view)
     RecyclerView mediaContainerRecyclerView;
 
@@ -27,15 +30,30 @@ public class MediaSelectionFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        populateStandingRecyclerView();
+        populateMediaSelectionRecyclerView();
         return inflater.inflate(R.layout.fragment_media_selection, container, false);
     }
 
-    public void populateStandingRecyclerView() {
+    public void populateMediaSelectionRecyclerView() {
         MediaSelectionAdapter mediaSelectionAdapter = new MediaSelectionAdapter(getActivity());
         LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false);
         mediaContainerRecyclerView.setLayoutManager(layoutManager);
         mediaContainerRecyclerView.setAdapter(mediaSelectionAdapter);
+    }
+
+    @OnClick({R.id.photos_text_view, R.id.stickers_text_view, R.id.gifs_text_view})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.photos_text_view:
+                photosTextView.setBackgroundColor(getResources().getColor(R.color.white));
+                break;
+            case R.id.stickers_text_view:
+                stickersTextView.setBackgroundColor(getResources().getColor(R.color.white));
+                break;
+            case R.id.gifs_text_view:
+                gifsTextView.setBackgroundColor(getResources().getColor(R.color.white));
+                break;
+        }
 
     }
 }
