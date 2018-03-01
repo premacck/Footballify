a9b09328610a81165132f0a222766b29aa2f54f8
Hasan Shaikh <hasan@plank.life>
2018-03-01 18:55:49 +0530

    JUNA-1350 #comment comments addressed

--- app/src/main/java/life/plank/juna/zone/util/NetworkStatus.java
+++ app/src/main/java/life/plank/juna/zone/util/NetworkStatus.java
@@ -5,18 +5,19 @@ import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.widget.Toast;
 
+import life.plank.juna.zone.R;
+
 /**
  * Created by plank-hasan on 3/1/2018.
  */
 
 public class NetworkStatus {
     public static boolean checkNetworkStatus(Context context) {
-
         @SuppressWarnings("static-access")
         NetworkInfo networkInfo = ((ConnectivityManager) context.getSystemService(context
                 .CONNECTIVITY_SERVICE)).getActiveNetworkInfo();
         if (networkInfo == null || !networkInfo.isConnected()) {
-            Toast.makeText(context, "Please connect to internet", Toast.LENGTH_SHORT).show();
+            Toast.makeText(context, context.getString(R.string.cannot_connect_to_the_internet), Toast.LENGTH_SHORT).show();
             return false;
         }
         return true;
---
+++ app/src/test/java/life/plank/juna/zone/view/activity/SwipePageActivityTest.java
@@ -0,0 +1,9 @@
+package life.plank.juna.zone.view.activity;
+
+/**
+ * Created by plank-hasan on 3/1/2018.
+ */
+
+public class SwipePageActivityTest {
+    //TODO: instrumental test cases needs to be added for spiners,liveZone button,News feeds grid and drawer
+}
