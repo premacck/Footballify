382432a52a724c548561e8d55fc13b62caac3741
Hasan Shaikh <hasan@plank.life>
2018-03-07 16:07:55 +0530

    JUNA-1321 #comment delay added before each item gets added to the adapter.

--- app/src/main/java/life/plank/juna/zone/util/AppConstants.java
+++ app/src/main/java/life/plank/juna/zone/util/AppConstants.java
@@ -8,7 +8,7 @@ public class AppConstants {
     public static final String FOOTBALL_FEEDS_HEADER_KEY = "newsfeed-continuation-token";
     public static final String DEFAULT_ERROR_MESSAGE = "Something went wrong, Please try again later!";
     public static final String PINNED_FEEDS = "pinnedfeeds";
-    public static final int DELAY = 200;
+    public static final int DELAY = 1200;
     public static final int PAGINATION_DELAY = 1000;
     public static final int SPINNER_DIALOG_WIDTH = 150;
     public static final String POSITION = "position";
