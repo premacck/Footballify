26b02e6c8a7776bc5632ac43ed329eb5c39741be
PLANK\plank-sharath <sharath@plank.life>
2018-03-09 20:54:18 +0530

    JUNA-1315 #comment * scores and fixture implementation

--- app/src/main/java/life/plank/juna/zone/data/network/model/ScoreFixture.java
+++ app/src/main/java/life/plank/juna/zone/data/network/model/ScoreFixture.java
@@ -29,14 +29,14 @@ public class ScoreFixture {
 
     public static List<ScoreFixture> getFixture(Context context) {
         List<ScoreFixture> fixtureModelList = new ArrayList<>();
-        fixtureModelList.add(new ScoreFixture("header", context.getString(R.string.header_fa_cup), context.getString(R.string.round_fa_cup), context.getString(R.string.teamone_fa_cup), context.getString(R.string.teamtwo_fa_cup), "" + context.getString(R.string.time_fa_cup)));
-        fixtureModelList.add(new ScoreFixture("body", context.getString(R.string.header_fa_cup), context.getString(R.string.round_fa_cup), context.getString(R.string.teamone_fa_cup), context.getString(R.string.teamtwo_fa_cup), "" + context.getString(R.string.time_fa_cup)));
+        fixtureModelList.add(new ScoreFixture("header", context.getString(R.string.header_fa_cup), context.getString(R.string.round_fa_cup), context.getString(R.string.team_one_fa_cup), context.getString(R.string.team_two_fa_cup), "" + context.getString(R.string.time_fa_cup)));
+        fixtureModelList.add(new ScoreFixture("body", context.getString(R.string.header_fa_cup), context.getString(R.string.round_fa_cup), context.getString(R.string.team_one_fa_cup), context.getString(R.string.team_two_fa_cup), "" + context.getString(R.string.time_fa_cup)));
         fixtureModelList.add(new ScoreFixture("header", context.getString(R.string.header_league), " ", context.getString(R.string.teamone_league), context.getString(R.string.teamtwo_league), context.getString(R.string.time_league)));
         fixtureModelList.add(new ScoreFixture("body", context.getString(R.string.header_league), " ", context.getString(R.string.teamone_league), context.getString(R.string.teamtwo_league), context.getString(R.string.time_league)));
-        fixtureModelList.add(new ScoreFixture("body", context.getString(R.string.header_south), " ", context.getString(R.string.teamone_south), context.getString(R.string.teamtwo_south), context.getString(R.string.time_south)));
-        fixtureModelList.add(new ScoreFixture("header", context.getString(R.string.header_fa_cup), context.getString(R.string.round_fa_cup), context.getString(R.string.teamone_fa_cup), context.getString(R.string.teamtwo_fa_cup), "" + context.getString(R.string.time_fa_cup)));
+        fixtureModelList.add(new ScoreFixture("body", context.getString(R.string.header_south), " ", context.getString(R.string.team_one_south), context.getString(R.string.team_two_south), context.getString(R.string.time_south)));
+        fixtureModelList.add(new ScoreFixture("header", context.getString(R.string.header_fa_cup), context.getString(R.string.round_fa_cup), context.getString(R.string.team_one_fa_cup), context.getString(R.string.team_two_fa_cup), "" + context.getString(R.string.time_fa_cup)));
         fixtureModelList.add(new ScoreFixture("body", context.getString(R.string.header_league), " ", context.getString(R.string.teamone_league), context.getString(R.string.teamtwo_league), context.getString(R.string.time_league)));
-        fixtureModelList.add(new ScoreFixture("body", context.getString(R.string.header_south), " ", context.getString(R.string.teamone_south), context.getString(R.string.teamtwo_south), context.getString(R.string.time_south)));
+        fixtureModelList.add(new ScoreFixture("body", context.getString(R.string.header_south), " ", context.getString(R.string.team_one_south), context.getString(R.string.team_two_south), context.getString(R.string.time_south)));
         return fixtureModelList;
     }
 
