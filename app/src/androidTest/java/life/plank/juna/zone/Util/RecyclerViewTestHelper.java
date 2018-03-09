b864e883825ce3d87f869dd10fdd56d988b24500
Niraj Kulal <niraj@plank.life>
2018-03-09 18:49:05 +0530

    JUNA-1394 #comment removed unwanted codes

--- app/src/androidTest/java/life/plank/juna/zone/Util/RecyclerViewTestHelper.java
+++ app/src/androidTest/java/life/plank/juna/zone/Util/RecyclerViewTestHelper.java
@@ -1,10 +1,6 @@
 package life.plank.juna.zone.Util;
 
-import android.support.test.espresso.intent.Checks;
-import android.support.test.espresso.matcher.BoundedMatcher;
 import android.view.View;
-import android.widget.EditText;
-import android.widget.TextView;
 
 import org.hamcrest.Description;
 import org.hamcrest.Matcher;
@@ -29,21 +25,6 @@ public class RecyclerViewTestHelper {
             @Override
             public boolean matchesSafely(View view) {
                 return matcher.matches(view) && currentIndex++ == index;
-            }
-        };
-    }
-
-    public static Matcher<View> withTextColor(final int color) {
-        Checks.checkNotNull(color);
-        return new BoundedMatcher<View, TextView>(EditText.class) {
-            @Override
-            public boolean matchesSafely(TextView textView) {
-                textView.getCurrentTextColor();
-                return color == textView.getCurrentTextColor();
-            }
-            @Override
-            public void describeTo(Description description) {
-                description.appendText("with text color: ");
             }
         };
     }
