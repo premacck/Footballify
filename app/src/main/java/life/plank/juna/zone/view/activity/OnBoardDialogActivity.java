9fe400a138df574375c007e516e747242c01b50a
Hasan Shaikh <hasan@plank.life>
2018-03-13 14:54:35 +0530

    JUNA-1414 #comment switch case condtion merged

--- app/src/main/java/life/plank/juna/zone/view/activity/OnBoardDialogActivity.java
+++ app/src/main/java/life/plank/juna/zone/view/activity/OnBoardDialogActivity.java
@@ -274,10 +274,7 @@ public class OnBoardDialogActivity extends AppCompatActivity implements View.OnC
                     idp.retrieveConfig(this, retrieveCallback);
                 }
             }
-            case R.id.drawer_linear_layout: {
-                drawerLayout.closeDrawer(GravityCompat.END);
-                break;
-            }
+            case R.id.drawer_linear_layout: 
             case R.id.drawer_transparent_view: {
                 drawerLayout.closeDrawer(GravityCompat.END);
                 break;
