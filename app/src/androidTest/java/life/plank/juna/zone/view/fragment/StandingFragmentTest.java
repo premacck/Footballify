e1f8837c1bb6e0a29594c0f3ea542c20bb96657d
PLANK\plank-prachi <prachi@plank.life>
2018-04-13 14:02:59 +0530

    JUNA-1576 #comment IdlingResources waiting Time added

--- app/src/androidTest/java/life/plank/juna/zone/view/fragment/StandingFragmentTest.java
+++ app/src/androidTest/java/life/plank/juna/zone/view/fragment/StandingFragmentTest.java
@@ -1,8 +1,9 @@
 package life.plank.juna.zone.view.fragment;
 
 import android.content.res.Resources;
+import android.support.test.espresso.Espresso;
+import android.support.test.espresso.IdlingResource;
 import android.support.test.espresso.contrib.RecyclerViewActions;
-import android.support.test.espresso.matcher.ViewMatchers;
 import android.support.test.filters.LargeTest;
 import android.support.test.runner.AndroidJUnit4;
 
@@ -13,7 +14,7 @@ import org.junit.runner.RunWith;
 
 import life.plank.juna.zone.FragmentTestRule;
 import life.plank.juna.zone.R;
-import life.plank.juna.zone.Util.RecyclerViewItemCountAssertion;
+import life.plank.juna.zone.Util.ElapsedTimeIdlingResource;
 import life.plank.juna.zone.view.adapter.StandingTableAdapter;
 
 import static android.support.test.espresso.Espresso.onView;
@@ -36,101 +37,121 @@ public class StandingFragmentTest {
     public FragmentTestRule<StandingFragment> fragmentTestRule = new FragmentTestRule<>(StandingFragment.class);
     StandingTableAdapter standingTableAdapter;
     private Resources resource;
+    private int waitingTime = 300;
+    private IdlingResource idlingResource;
 
     @Before
     public void setUp() {
         fragmentTestRule.launchActivity(null);
         resource = fragmentTestRule.getActivity().getResources();
+        idlingResource = new ElapsedTimeIdlingResource(waitingTime);
+
     }
 
     @Test
     public void launchOfStandingFragmentShouldDisplayCloseImageView() {
         /*launch StandingFragment
-        * check if cancel image view is displayed*/
+         * check if cancel image view is displayed*/
+        Espresso.registerIdlingResources(idlingResource);
         onView(withId(R.id.cancel_image_view)).check(matches(isDisplayed()));
+        Espresso.unregisterIdlingResources(idlingResource);
     }
 
     @Test
     public void launchOfStandingFragmentShouldDisplaySearchBar() {
+        Espresso.registerIdlingResources(idlingResource);
         /*launch StandingFragment check if searchBar  view is displayed*/
         onView(withId(R.id.search_bar)).check(matches(isDisplayed()));
+        Espresso.unregisterIdlingResources(idlingResource);
     }
 
     @Test
     public void standingRecyclerViewShouldScroll() {
+        Espresso.registerIdlingResources(idlingResource);
         /*check if standingRecyclerView scrolls*/
         onView(withId(R.id.table_recycler_view)).perform(RecyclerViewActions.scrollToPosition(1));
+        Espresso.unregisterIdlingResources(idlingResource);
     }
 
     @Test
     public void standingRecyclerViewItemShouldDisplaySerialNumberTextView() {
         /*check if standingFragment RecyclerView  item displays */
+        Espresso.registerIdlingResources(idlingResource);
         standingRecyclerViewShouldScroll();
         onView(withIndex(withId(R.id.serial_number_text_view), 1)).check(matches(withText(resource.getString(R.string.serial_number))));
+        Espresso.unregisterIdlingResources(idlingResource);
+
     }
 
     @Test
     public void launchOfStandingFragmentShouldDisplayTeamName() {
         /*launch StandingFragment fragmnet
-        * check if TeamName is displayed*/
+         * check if TeamName is displayed*/
+        Espresso.registerIdlingResources(idlingResource);
         standingRecyclerViewShouldScroll();
         onView(withIndex(withId(R.id.team_name_text_view), 1)).check(matches((isDisplayed())));
+        Espresso.unregisterIdlingResources(idlingResource);
     }
 
     @Test
     public void launchOfStandingFragmentShouldDisplayTotalPlayedMatch() {
         /*launch Standing fragment
-        * check if TotalPlayedMatch  is displayed*/
+         * check if TotalPlayedMatch  is displayed*/
+        Espresso.registerIdlingResources(idlingResource);
         standingRecyclerViewShouldScroll();
         onView(withIndex(withId(R.id.played_text_view), 1)).check(matches((isDisplayed())));
+        Espresso.unregisterIdlingResources(idlingResource);
+
     }
 
     @Test
     public void launchOfStandingFragmentShouldDisplayTotalWinMatch() {
         /*launch Standing fragment
-        * check if TotalWin In a match is displayed*/
+         * check if TotalWin In a match is displayed*/
+        Espresso.registerIdlingResources(idlingResource);
         standingRecyclerViewShouldScroll();
         onView(withIndex(withId(R.id.win_text_view), 1)).check(matches((isDisplayed())));
+        Espresso.unregisterIdlingResources(idlingResource);
     }
 
     @Test
     public void launchOfStandingFragmentShouldDisplayTotalDrawMatch() {
         /*launch Standing fragment
-        * check if draw textView  is displayed*/
+         * check if draw textView  is displayed*/
+        Espresso.registerIdlingResources(idlingResource);
         standingRecyclerViewShouldScroll();
         onView(withIndex(withId(R.id.draw_text_view), 1)).check(matches((isDisplayed())));
+        Espresso.unregisterIdlingResources(idlingResource);
     }
 
     @Test
     public void launchOfStandingFragmentShouldDisplayTotalLossInMatch() {
         /*launch Standing fragment
-        * check if Loss textView  is displayed*/
+         * check if Loss textView  is displayed*/
+        Espresso.registerIdlingResources(idlingResource);
         standingRecyclerViewShouldScroll();
         onView(withIndex(withId(R.id.loss_text_view), 1)).check(matches((isDisplayed())));
+        Espresso.unregisterIdlingResources(idlingResource);
     }
 
     @Test
     public void launchOfStandingFragmentShouldDisplaGoalDifference() {
         /*launch Standing fragment
-        * check if GoalDifference textView  is displayed*/
+         * check if GoalDifference textView  is displayed*/
+        Espresso.registerIdlingResources(idlingResource);
         standingRecyclerViewShouldScroll();
         onView(withIndex(withId(R.id.goal_difference_text_view), 1)).check(matches((isDisplayed())));
+        Espresso.unregisterIdlingResources(idlingResource);
     }
 
     @Test
     public void launchOfStandingFragmentShouldDisplaPointTable() {
         /*launch Standing fragment
-        * check if PointTable textView  is displayed*/
+         * check if PointTable textView  is displayed*/
+        Espresso.registerIdlingResources(idlingResource);
         standingRecyclerViewShouldScroll();
         onView(withIndex(withId(R.id.point_table_text_view), 1)).check(matches((isDisplayed())));
-    }
-
-    @Test
-    public void standingFragmentRecyclerViewCount() {
-        /*check if recyclerView is displayed
-        * check the recyclerView item count*/
-        onView(ViewMatchers.withId(R.id.table_recycler_view)).check(matches(isDisplayed()));
-        onView(withId(R.id.table_recycler_view)).check(new RecyclerViewItemCountAssertion(10));
+        Espresso.unregisterIdlingResources(idlingResource);
     }
 
     // Types a message into a EditText element.
