683691ce4c468fb6fa2b4fb1e55bd2070fb3df95
PLANK\plank-prachi <prachi@plank.life>
2018-04-16 11:39:28 +0530

    JUNA-1597 #comment code optimisation

--- app/src/main/java/life/plank/juna/zone/view/adapter/StandingTableAdapter.java
+++ app/src/main/java/life/plank/juna/zone/view/adapter/StandingTableAdapter.java
@@ -3,24 +3,18 @@ package life.plank.juna.zone.view.adapter;
 import android.content.Context;
 import android.graphics.Typeface;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Filter;
-import android.widget.Filterable;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import life.plank.juna.zone.R;
 import life.plank.juna.zone.data.network.model.StandingModel;
-
-import static rx.plugins.RxJavaHooks.clear;
 
 /**
  * Created by plank-prachi on 1/30/2018.
@@ -116,6 +110,5 @@ public class StandingTableAdapter extends RecyclerView.Adapter<StandingTableAdap
             super(itemView);
             ButterKnife.bind(this, itemView);
         }
-
     }
 }
