d26565cdfb5db2219dad76647484da2c7f8f923f
Hasan Shaikh <hasan@plank.life>
2018-04-18 15:54:35 +0530

    JUNA-1622 #comment date format added and font styling done as per the design

--- app/src/main/java/life/plank/juna/zone/util/AppConstants.java
+++ app/src/main/java/life/plank/juna/zone/util/AppConstants.java
@@ -34,4 +34,9 @@ public class AppConstants {
     public static final String FILE_PROVIDER_TO_CAPTURE_IMAGE = "life.plank.juna.zone.fileprovider";
     public static final int CAMERA_IMAGE_RESULT = 5;
     public static final String CACHE_FILE_NAME = "feeditems.txt";
+    public static final String DATE_FORMAT_FEED_DETAILS = "yyyy-MM-dd'T'HH:mm:ss'Z'";
+    public static final String DAY_FORMAT = "dd";
+    public static final String MONTH_FORMAT = "MMM";
+    public static final String YEAR_FORMAT = "yyyy";
+    public static final String TIME_FORMAT = "h:mm a";
 }
