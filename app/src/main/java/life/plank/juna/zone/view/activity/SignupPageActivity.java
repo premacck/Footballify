fdfbd95402b99ab52b2dc9f23168a1fb432a8f92
PLANK\plank-sharath <sharath@plank.life>
2018-04-25 10:30:12 +0530

    JUNA-1647 #comment * ui designs for signup

---
+++ app/src/main/java/life/plank/juna/zone/view/activity/SignupPageActivity.java
@@ -0,0 +1,15 @@
+package life.plank.juna.zone.view.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import life.plank.juna.zone.R;
+
+public class SignupPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup_page);
+    }
+}
