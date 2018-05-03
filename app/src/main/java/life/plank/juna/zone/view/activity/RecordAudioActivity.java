36fc1f6ac31bce2438908939d025117b71902fa1
PLANK\plank-sharath <sharath@plank.life>
2018-05-03 23:44:54 +0530

    JA-40 #comment recording audio

---
+++ app/src/main/java/life/plank/juna/zone/view/activity/RecordAudioActivity.java
@@ -0,0 +1,13 @@
+package life.plank.juna.zone.view.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RecordAudioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_record_audio);
+    }
+}
