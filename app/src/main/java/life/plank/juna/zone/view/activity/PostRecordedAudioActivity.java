19c2aa87e90c87205431425899e323a085bccdb7
PLANK\plank-sharath <sharath@plank.life>
2018-05-04 01:10:27 +0530

    JA-40 #comment * audio recording ui and implementation

--- app/src/main/java/life/plank/juna/zone/view/activity/PostRecordedAudioActivity.java
+++ app/src/main/java/life/plank/juna/zone/view/activity/PostRecordedAudioActivity.java
@@ -1,25 +1,78 @@
 package life.plank.juna.zone.view.activity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
-
+import android.widget.ImageView;
+import java.io.IOException;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import life.plank.juna.zone.R;
 
-public class PostRecordedAudioActivity extends AppCompatActivity {
+public class PostRecordedAudioActivity extends AppCompatActivity implements View.OnClickListener {
+    private static final String LOG_TAG = "PostRecordingActivity";
     String audioFile;
+    @BindView(R.id.play_button)
+    ImageView playImageView;
+    private MediaPlayer player = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post_recorded_audio);
+        ButterKnife.bind(this);
         Bundle extras = getIntent().getExtras();
         if (extras == null) {
             audioFile = null;
         } else {
             audioFile = extras.getString("audiopath");
+        }
+        playImageView.setOnClickListener(this);
+    }
+
+    private void onPlay(boolean start) {
+        if (start) {
+            startPlaying();
+        } else {
+            stopPlaying();
+        }
+    }
+
+    private void startPlaying() {
+        player = new MediaPlayer();
+        try {
+            player.setDataSource(audioFile);
+            player.prepare();
+            player.start();
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "prepare() failed");
+        }
+    }
+
+    private void stopPlaying() {
+        player.release();
+        player = null;
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (player != null) {
+            player.release();
+            player = null;
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        boolean startPlaying = true;
+        if (view.isSelected()) {
+            stopPlaying();
+        } else {
+            view.setSelected(true);
+            onPlay(startPlaying);
         }
     }
 }
--- app/src/main/java/life/plank/juna/zone/view/activity/RecordAudioActivity.java
+++ app/src/main/java/life/plank/juna/zone/view/activity/RecordAudioActivity.java
@@ -4,43 +4,42 @@ import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.media.MediaRecorder;
-import android.os.Environment;
+import android.os.Bundle;
 import android.os.SystemClock;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Chronometer;
 import android.widget.ImageButton;
 import android.widget.ImageView;
-
-import java.io.File;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import life.plank.juna.zone.R;
-import life.plank.juna.zone.util.AppConstants;
 
-public class RecordAudioActivity extends AppCompatActivity implements  View.OnTouchListener {
+public class RecordAudioActivity extends AppCompatActivity implements View.OnTouchListener {
     private static final String TAG = RecordAudioActivity.class.getSimpleName();
-     @BindView(R.id.start_image_button)
-     ImageButton startButton;
-     @BindView(R.id.chronometer_text_view)
-     Chronometer chronometer;
-    private boolean permissionToRecordAccepted = false;
     private static final int REQUEST_RECORD_AUDIO_PERMISSION = 200;
-    private MediaRecorder recorder = null;
     private static final String LOG_TAG = "RecordAudioActivity";
     private static String fileName = null;
+    @BindView(R.id.start_image_button)
+    ImageButton startButton;
+    @BindView(R.id.chronometer_text_view)
+    Chronometer chronometer;
     @BindView(R.id.audio_image_view)
     ImageView audioImageview;
-    private String[] permissions = {Manifest.permission.RECORD_AUDIO,Manifest.permission.WRITE_EXTERNAL_STORAGE};
+    @BindView(R.id.time_relative_layout)
+    RelativeLayout timeLayout;
+    @BindView(R.id.tap_to_hold_text_view)
+    TextView tapTextView;
+    private boolean permissionToRecordAccepted = false;
+    private MediaRecorder recorder = null;
+    private String[] permissions = {Manifest.permission.RECORD_AUDIO, Manifest.permission.WRITE_EXTERNAL_STORAGE};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,6 +53,7 @@ public class RecordAudioActivity extends AppCompatActivity implements  View.OnTo
         fileName = getExternalCacheDir().getAbsolutePath();
         fileName += "/audiorecordtest.3gp";
     }
+
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
@@ -64,22 +64,27 @@ public class RecordAudioActivity extends AppCompatActivity implements  View.OnTo
         }
         if (!permissionToRecordAccepted) finish();
     }
+
     @Override
     public boolean onTouch(View view, MotionEvent event) {
-        switch(event.getAction()) {
+        switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN:
+                timeLayout.setVisibility(View.VISIBLE);
+                tapTextView.setVisibility(View.INVISIBLE);
                 startRecording();
                 chronometer.setBase(SystemClock.elapsedRealtime());
                 chronometer.start();
                 return true;
             case MotionEvent.ACTION_UP:
                 stopRecording();
+                timeLayout.setVisibility(View.INVISIBLE);
+                tapTextView.setVisibility(View.VISIBLE);
                 chronometer.stop();
-                Intent in=new Intent(this,PostRecordedAudioActivity.class);
-                in.putExtra("audiopath",fileName);
+                Intent in = new Intent(this, PostRecordedAudioActivity.class);
+                in.putExtra("audiopath", fileName);
                 startActivity(in);
+                finish();
                 return true;
-
         }
         return false;
     }
@@ -98,25 +103,15 @@ public class RecordAudioActivity extends AppCompatActivity implements  View.OnTo
         recorder.start();
         audioImageview.setBackgroundResource(R.drawable.mic_red_color);
     }
+
     private void stopRecording() {
-        recorder.stop();
-        recorder.release();
-        recorder = null;
-        audioImageview.setBackgroundResource(R.drawable.mic_red);
+        try {
+            recorder.stop();
+            recorder.release();
+            recorder = null;
+            audioImageview.setBackgroundResource(R.drawable.mic_red);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
-
- public File CreateFile()throws IOException {
- {
-     String timeStamp = new SimpleDateFormat(AppConstants.DATE_FORMAT).format(new Date());
-     String imageFileName = AppConstants.RECORDED_AUDIO_NAME + timeStamp + AppConstants. RECORDED_AUDIO_FORMAT;
-     File mediaStorageDirectory = new File(Environment.getExternalStorageDirectory(),
-             AppConstants.CAPTURED_IMAGES_FOLDER_NAME);
-     File storageDirectory = new File(mediaStorageDirectory + File.separator + AppConstants.CAPTURED_IMAGES_SUB_FOLDER_NAME_AUDIO);
-     if (!storageDirectory.exists()) {
-         storageDirectory.mkdirs();
-     }
-     return new File(storageDirectory, imageFileName);
- }
- }
-
 }
