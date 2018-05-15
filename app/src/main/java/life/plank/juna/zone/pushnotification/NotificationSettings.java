032824bf365cf8ff4c936f29b6e7bd4f513ac85e
PLANK\plank-prachi <prachi@plank.life>
2018-05-15 17:25:21 +0530

    JA-210 #comment Addressed

--- app/src/main/java/life/plank/juna/zone/pushnotification/NotificationSettings.java
+++ app/src/main/java/life/plank/juna/zone/pushnotification/NotificationSettings.java
@@ -1,15 +1,12 @@
 package life.plank.juna.zone.pushnotification;
 
-import life.plank.juna.zone.R;
-import life.plank.juna.zone.view.activity.SplashScreenActivity;
-import life.plank.juna.zone.view.activity.SwipePageActivity;
-
 /**
  * Created by plank-dhamini on 18/11/17.
  */
 
 public class NotificationSettings {
-    public static String hubListenConnectionString  = "Endpoint=sb://footballworkflows.servicebus.windows.net/;SharedAccessKeyName=DefaultListenSharedAccessSignature;SharedAccessKey=ywU+kaiYwofBfWDB3IiW/rbiVnQPEQQAlpmHz6pJgMU=";
+    //Todo: Externalize this configuration
+    public static String hubListenConnectionString = "Endpoint=sb://footballworkflows.servicebus.windows.net/;SharedAccessKeyName=DefaultListenSharedAccessSignature;SharedAccessKey=ywU+kaiYwofBfWDB3IiW/rbiVnQPEQQAlpmHz6pJgMU=";
     public static String senderId = "1061001784593";
     public static String hubName = "juna-app";
 }
