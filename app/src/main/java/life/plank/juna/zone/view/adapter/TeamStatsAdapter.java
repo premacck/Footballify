4cd8db68f2d77e65efa2830c57c00e2731bdd2d9
PLANK\plank-prachi <prachi@plank.life>
2018-05-22 19:18:06 +0530

    JA-215 #comment added push notification

--- app/src/main/java/life/plank/juna/zone/view/adapter/TeamStatsAdapter.java
+++ app/src/main/java/life/plank/juna/zone/view/adapter/TeamStatsAdapter.java
@@ -9,12 +9,16 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.squareup.picasso.Picasso;
+
 import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import life.plank.juna.zone.R;
 import life.plank.juna.zone.data.network.model.TeamStatsModel;
+import life.plank.juna.zone.util.RoundedTransformation;
+import life.plank.juna.zone.util.UIDisplayUtil;
 
 /**
  * Created by plank-prachi on 1/30/2018.
@@ -44,7 +48,6 @@ public class TeamStatsAdapter extends RecyclerView.Adapter<TeamStatsAdapter.Team
         holder.teamsStatsGoalAgaintsTextView.setText( String.valueOf(teamStatsModelList.get( position ).getTotalGoalsAgainst()) );
         holder.teamStatsSerialNumber.setText( String.valueOf(teamStatsModelList.get( position ).getId()));
         holder.teamStatsTeamNameTextView.setText( String.valueOf(teamStatsModelList.get( position ).getFootballTeam().getName()) );
-
     }
 
     @Override
