69d0b14b21deb574f91f611ea76e2cddbd92eb01
PLANK\plank-prachi <prachi@plank.life>
2018-05-25 19:04:09 +0530

    JA-225 #comment Board Api Call Component added

--- app/src/main/java/life/plank/juna/zone/view/activity/BoardActivity.java
+++ app/src/main/java/life/plank/juna/zone/view/activity/BoardActivity.java
@@ -2,39 +2,109 @@ package life.plank.juna.zone.view.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v8.renderscript.RenderScript;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bvapp.arcmenulibrary.ArcMenu;
 import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.messaging.FirebaseMessaging;
 
+import java.net.HttpURLConnection;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import life.plank.juna.zone.R;
+import life.plank.juna.zone.ZoneApplication;
+import life.plank.juna.zone.data.network.interfaces.RestApi;
+import life.plank.juna.zone.data.network.model.FootballFeed;
+import life.plank.juna.zone.util.AppConstants;
+import life.plank.juna.zone.util.NetworkStateReceiver;
 import life.plank.juna.zone.view.adapter.BoardMediaAdapter;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import rx.Observer;
+import rx.Subscription;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
 
 /**
  * Created by plank-hasan on 5/3/2018.
  */
 
 public class BoardActivity extends AppCompatActivity {
-
+    @Inject
+    @Named("azure")
+    Retrofit retrofit;
+    int TRCNumber = 20;
     @BindView(R.id.board_recycler_view)
     RecyclerView boardRecyclerView;
     @BindView(R.id.board_arc_menu)
     ArcMenu arcMenu;
     @BindView(R.id.following_text_view)
     TextView followingTextView;
+    @BindView(R.id.parent_layout)
+    RelativeLayout parentLayout;
+    BoardMediaAdapter boardMediaAdapter;
+    GridLayoutManager gridLayoutManager;
+    private int apiHitCount = 0;
+    private List<FootballFeed> footballFeeds;
+    private RestApi restApi;
+    private Subscription subscription;
+    private String nextPageToken = "";
+    private int PAGE_SIZE;
+    private boolean isLastPage = false;
+    private boolean isLoading = false;
+    private RecyclerView.OnScrollListener recyclerViewOnScrollListener = new RecyclerView.OnScrollListener() {
+        @Override
+        public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+            if (newState == RecyclerView.SCROLL_STATE_IDLE)
+                arcMenu.show();
+            super.onScrollStateChanged( recyclerView, newState );
+        }
+
+        @Override
+        public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+            super.onScrolled( recyclerView, dx, dy );
+            int visibleItemCount = gridLayoutManager.getChildCount();
+            int totalItemCount = gridLayoutManager.getItemCount();
+            int firstVisibleItemPosition = gridLayoutManager.findFirstVisibleItemPosition();
+            if (!isLoading && !isLastPage) {
+                if ((visibleItemCount + firstVisibleItemPosition) >= totalItemCount
+                        && firstVisibleItemPosition >= 0
+                        && totalItemCount >= PAGE_SIZE) {
+                    isLoading = true;
+                    new Handler().postDelayed( new Runnable() {
+                        @Override
+                        public void run() {
+                            getBoardApiCall();
+
+                        }
+                    }, AppConstants.PAGINATION_DELAY );
+                }
+            }
+            if (dy > 0 || dy < 0 && arcMenu.isShown())
+                arcMenu.hide();
+        }
+    };
+    private NetworkStateReceiver networkStateReceiver;
+    private RenderScript renderScript;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -42,19 +112,79 @@ public class BoardActivity extends AppCompatActivity {
         Log.e( "Device ID", FirebaseInstanceId.getInstance().getToken() );
         setContentView( R.layout.activity_board );
         ButterKnife.bind( this );
+        ((ZoneApplication) getApplication()).getBoardFeedNetworkComponent().inject( this );
+        restApi = retrofit.create( RestApi.class );
+        getBoardApiCall();
         initRecyclerView();
         setUpBoomMenu();
     }
 
     //todo: Inject adapter
     private void initRecyclerView() {
-        GridLayoutManager gridLayoutManager = new GridLayoutManager( this, 4, GridLayoutManager.VERTICAL, false );
+        footballFeeds = new ArrayList<>();
+
+        gridLayoutManager = new GridLayoutManager( this, 4, GridLayoutManager.VERTICAL, false );
         boardRecyclerView.setLayoutManager( gridLayoutManager );
-        BoardMediaAdapter boardMediaAdapter = new BoardMediaAdapter( this );
+        boardMediaAdapter = new BoardMediaAdapter( this ,footballFeeds);
         boardRecyclerView.setAdapter( boardMediaAdapter );
         boardRecyclerView.setHasFixedSize( true );
+        boardRecyclerView.addOnScrollListener( recyclerViewOnScrollListener );
+        renderScript = RenderScript.create( this );
+
         /*footballFeedAdapter.setPinFeedListener(this);
         footballFeedAdapter.setOnLongPressListener(this);*/
+    }
+
+
+    public String updateToken(String nextPageToken, String replaceStringRT, String replaceStringTRC) {
+        if (!nextPageToken.isEmpty()) {
+            String updatedNextPageToken = nextPageToken.replaceFirst( AppConstants.REGULAR_EXPRESSION_RT, replaceStringRT );
+            updatedNextPageToken = updatedNextPageToken.replaceFirst( AppConstants.REGULAR_EXPRESSION_TRC, replaceStringTRC );
+            return updatedNextPageToken;
+        }
+        return "";
+    }
+
+    public void getBoardApiCall() {
+        subscription = restApi.getBoardFeed( updateToken( nextPageToken,
+                getString( R.string.replace_rt ) +
+                        String.valueOf( apiHitCount ), getString( R.string.replace_trc )
+                        + String.valueOf( apiHitCount * TRCNumber ) ) )
+                .subscribeOn( Schedulers.io() )
+                .observeOn( AndroidSchedulers.mainThread() )
+                .subscribe( new Observer<Response<List<FootballFeed>>>() {
+                    @Override
+                    public void onCompleted() {
+                        Log.d( "", "In onCompleted()" );
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        Log.d( "", "In onCompleted()" );
+                    }
+
+                    public void onNext(Response<List<FootballFeed>> response) {
+                        if (response.code() == HttpURLConnection.HTTP_OK) {
+                            if (apiHitCount == 0) {
+                                nextPageToken = response.headers().get( AppConstants.FOOTBALL_FEEDS_HEADER_KEY );
+                            }
+                            setUpAdapterWithNewData( response.body() );
+                            apiHitCount = apiHitCount + 1;
+                        } else {
+                            Toast.makeText( BoardActivity.this, "Error message", Toast.LENGTH_SHORT ).show();
+                        }
+                    }
+                } );
+    }
+
+    private void setUpAdapterWithNewData(List<FootballFeed> footballFeedsList) {
+        if (!footballFeedsList.isEmpty() && footballFeedsList.size() > 0) {
+            if ("".contentEquals( nextPageToken ) ? (isLastPage = true) : (isLoading = false)) ;
+            footballFeeds.addAll( footballFeedsList );
+            boardMediaAdapter.notifyDataSetChanged();
+            PAGE_SIZE = footballFeedsList.size();
+
+        }
     }
 
     public void setUpBoomMenu() {
@@ -62,11 +192,11 @@ public class BoardActivity extends AppCompatActivity {
         arcMenu.setIcon( R.drawable.ic_un, R.drawable.ic_close_white );
         int[] fabImages = {R.drawable.ic_settings_white,
                 R.drawable.ic_person, R.drawable.ic_home_purple, R.drawable.ic_gallery,
-                R.drawable.ic_camera_white, R.drawable.ic_mic, R.drawable.ic_link,R.drawable.ic_video};
+                R.drawable.ic_camera_white, R.drawable.ic_mic, R.drawable.ic_link, R.drawable.ic_video};
         int[] backgroundColors = {R.drawable.fab_circle_background_grey,
                 R.drawable.fab_circle_background_grey, R.drawable.fab_circle_background_white, R.drawable.fab_circle_background_pink,
-                R.drawable.fab_circle_background_pink, R.drawable.fab_circle_background_pink, R.drawable.fab_circle_background_pink,R.drawable.fab_circle_background_pink};
-        String[] titles = {"Settings", "Profile", "Home", "Gallery", "Camera", "Audio", "Attachment","Video"};
+                R.drawable.fab_circle_background_pink, R.drawable.fab_circle_background_pink, R.drawable.fab_circle_background_pink, R.drawable.fab_circle_background_pink};
+        String[] titles = {"Settings", "Profile", "Home", "Gallery", "Camera", "Audio", "Attachment", "Video"};
         for (int i = 0; i < fabImages.length; i++) {
             View child = getLayoutInflater().inflate( R.layout.layout_floating_action_button, null );
             //child.setId(i);
@@ -111,7 +241,8 @@ public class BoardActivity extends AppCompatActivity {
                         }
                         case 6: {
                             break;
-                        } case 7: {
+                        }
+                        case 7: {
                             Intent intent = new Intent( BoardActivity.this, CameraActivity.class );
                             intent.putExtra( "OPEN_FROM", "Video" );
                             intent.putExtra( "API", "BoardActivity" );
--- app/src/main/java/life/plank/juna/zone/view/adapter/BoardMediaAdapter.java
+++ app/src/main/java/life/plank/juna/zone/view/adapter/BoardMediaAdapter.java
@@ -7,19 +7,26 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import life.plank.juna.zone.R;
+import life.plank.juna.zone.data.network.model.FootballFeed;
+import life.plank.juna.zone.util.RoundedTransformation;
+import life.plank.juna.zone.util.UIDisplayUtil;
 
 /**
  * Created by plank-prachi on 4/10/2018.
  */
 public class BoardMediaAdapter extends RecyclerView.Adapter<BoardMediaAdapter.BoardMediaViewHolder> {
     private Context context;
-
-
-    public BoardMediaAdapter(Context context) {
+    List<FootballFeed> footballFeeds;
+    public BoardMediaAdapter(Context context, List<FootballFeed> footballFeeds) {
         this.context = context;
+        this.footballFeeds = footballFeeds;
     }
 
     @Override
@@ -30,12 +37,31 @@ public class BoardMediaAdapter extends RecyclerView.Adapter<BoardMediaAdapter.Bo
 
     @Override
     public void onBindViewHolder(BoardMediaViewHolder holder, int position) {
+        FootballFeed footballFeed = footballFeeds.get(position);
+        if (footballFeed.getThumbnail() != null) {
+            Picasso.with(context)
+                    .load(footballFeed.getThumbnail().getImageUrl())
+                    .fit().centerCrop()
+                    .placeholder(R.drawable.ic_place_holder)
+                    .transform(new RoundedTransformation( UIDisplayUtil.dpToPx(8, context), 0))
+                    .error(R.drawable.ic_place_holder)
+                    .into(holder.tileImageView);
+        } else {
+            holder.tileImageView.setImageResource(R.drawable.ic_place_holder);
+        }
 
+    }
+    public  void setFootballFeedList(List<FootballFeed> footballFeeds) {
+        if (footballFeeds == null) {
+            return;
+        }
+        footballFeeds.addAll(footballFeeds);
+        notifyDataSetChanged();
     }
 
     @Override
     public int getItemCount() {
-        return 10;
+        return footballFeeds.size();
     }
 
     public class BoardMediaViewHolder extends RecyclerView.ViewHolder {
