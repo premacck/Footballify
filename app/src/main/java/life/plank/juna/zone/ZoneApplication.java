62a9d40c1d9b53be084bf1e9cd3a37ef85d158ce
plank-dhamini <dhamini@plank.life>
2018-07-05 15:49:06 +0530

    JA-318 #comment removed unused layouts

--- app/src/main/java/life/plank/juna/zone/ZoneApplication.java
+++ app/src/main/java/life/plank/juna/zone/ZoneApplication.java
@@ -4,14 +4,10 @@ import android.app.Application;
 import android.content.Context;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import life.plank.juna.zone.data.network.dagger.BoardFeedNetworkComponent;
 import life.plank.juna.zone.data.network.dagger.BoardItemLikeNetworkComponent;
-import life.plank.juna.zone.data.network.dagger.DaggerPostCommentFeedNetworkComponent;
-import life.plank.juna.zone.data.network.dagger.PostCommentFeedNetworkComponent;
 import life.plank.juna.zone.data.network.dagger.DaggerBoardFeedNetworkComponent;
 import life.plank.juna.zone.data.network.dagger.DaggerBoardItemLikeNetworkComponent;
 import life.plank.juna.zone.data.network.dagger.DaggerFixtureAndResultNetworkComponent;
@@ -20,6 +16,7 @@ import life.plank.juna.zone.data.network.dagger.DaggerImageUploaderNetworkCompon
 import life.plank.juna.zone.data.network.dagger.DaggerLikeFeedNetworkComponent;
 import life.plank.juna.zone.data.network.dagger.DaggerLineupNetworkComponent;
 import life.plank.juna.zone.data.network.dagger.DaggerNewsFeedsNetworkComponent;
+import life.plank.juna.zone.data.network.dagger.DaggerPostCommentFeedNetworkComponent;
 import life.plank.juna.zone.data.network.dagger.DaggerSigninUserNetworkComponent;
 import life.plank.juna.zone.data.network.dagger.DaggerSignupUserNetworkComponent;
 import life.plank.juna.zone.data.network.dagger.DaggerStandingsNetworkComponent;
@@ -29,10 +26,10 @@ import life.plank.juna.zone.data.network.dagger.ImageUploaderNetworkComponent;
 import life.plank.juna.zone.data.network.dagger.LikeFeedNetworkComponent;
 import life.plank.juna.zone.data.network.dagger.LineupNetworkComponent;
 import life.plank.juna.zone.data.network.dagger.NewsFeedsNetworkComponent;
+import life.plank.juna.zone.data.network.dagger.PostCommentFeedNetworkComponent;
 import life.plank.juna.zone.data.network.dagger.SigninUserNetworkComponent;
 import life.plank.juna.zone.data.network.dagger.SignupUserNetworkComponent;
 import life.plank.juna.zone.data.network.dagger.StandingsNetworkComponent;
-import life.plank.juna.zone.data.network.model.JunaUser;
 import life.plank.juna.zone.data.network.module.RestServiceModule;
 
 /**
@@ -41,10 +38,6 @@ import life.plank.juna.zone.data.network.module.RestServiceModule;
 
 public class ZoneApplication extends Application {
 
-    public static Integer roundNumber = 0;
-    public static Map<JunaUser, Boolean> pointsGameResultMap = new HashMap<>();
-    public static Map<JunaUser, Boolean> suddenDeathGameResultMap = new HashMap<>();
-    public static Integer suddenDeathLivesRemaining = 5;
     public static List<String> selectedTeamsList = new ArrayList<>();
     private static ZoneApplication zoneApplication;
     private NewsFeedsNetworkComponent newsFeedsNetworkComponent;
