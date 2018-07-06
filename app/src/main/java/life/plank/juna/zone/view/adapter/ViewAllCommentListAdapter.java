b4e821e499f2a49ddcdacee1c45fc9a7e41d5834
PLANK\plank-prachi <prachi@plank.life>
2018-07-06 12:27:42 +0530

    JA-325 #comment  RecyclerView Added in Clickthrough View All Comment

---
+++ app/src/main/java/life/plank/juna/zone/view/adapter/ViewAllCommentListAdapter.java
@@ -0,0 +1,42 @@
+package life.plank.juna.zone.view.adapter;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import butterknife.ButterKnife;
+import life.plank.juna.zone.R;
+
+public class ViewAllCommentListAdapter extends RecyclerView.Adapter<ViewAllCommentListAdapter.ViewAllCommentListAdapterViewHolder> {
+
+    private Context context;
+
+    public ViewAllCommentListAdapter(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    public ViewAllCommentListAdapterViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view;
+        view = LayoutInflater.from(parent.getContext()).inflate(R.layout.comment_feed_detail_row, parent, false);
+        return new ViewAllCommentListAdapterViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewAllCommentListAdapterViewHolder holder, int position) {
+    }
+
+    @Override
+    public int getItemCount() {
+        return 10;
+    }
+
+    class ViewAllCommentListAdapterViewHolder extends RecyclerView.ViewHolder {
+        ViewAllCommentListAdapterViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+        }
+    }
+}
