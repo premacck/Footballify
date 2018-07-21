7a7bab1ea24483e5062d5a0091189e0ca94b7273
plank-dhamini <dhamini@plank.life>
2018-07-21 14:52:45 +0530

    JA-348 #comment Zone UI * Corrections to zone_ui * Got the view reviewed by kiran and addressed all of them * No outstanding design changes as of now * Changed the image opacity * Added background for zone title * Minor layout margin corrections * Changed follow image (imported svg instead of png WHY? the png image was not opaque) * Added new fonts * Updated layout font

--- app/src/main/java/life/plank/juna/zone/view/adapter/ZoneAdapter.java
+++ app/src/main/java/life/plank/juna/zone/view/adapter/ZoneAdapter.java
@@ -6,6 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import butterknife.BindView;
@@ -34,10 +35,25 @@ public class ZoneAdapter extends RecyclerView.Adapter<ZoneAdapter.ZoneViewHolder
         holder.zoneImageView.setOnClickListener(view -> {
             if (holder.followTick.getVisibility() == View.VISIBLE) {
                 holder.followTick.setVisibility(View.INVISIBLE);
-                holder.zoneImageView.setAlpha(255);
+                holder.zoneImageView.setAlpha(context.getResources().getInteger(R.integer.opaque));
+                holder.zoneTitle.setVisibility(View.VISIBLE);
+                holder.followersCount.setVisibility(View.VISIBLE);
+
+                ViewGroup.LayoutParams params = holder.zoneImageView.getLayoutParams();
+                params.width = holder.zoneImageView.getMeasuredWidth() + context.getResources().getInteger(R.integer.zone_grid_layout_param);
+                params.height = holder.zoneImageView.getMeasuredWidth() + context.getResources().getInteger(R.integer.zone_grid_layout_param);
+                holder.zoneImageView.setLayoutParams(params);
+
             } else {
                 holder.followTick.setVisibility(View.VISIBLE);
-                holder.zoneImageView.setAlpha(160);
+                holder.zoneImageView.setAlpha(context.getResources().getInteger(R.integer.visiblilty_160));
+                holder.zoneTitle.setVisibility(View.INVISIBLE);
+                holder.followersCount.setVisibility(View.INVISIBLE);
+                ViewGroup.LayoutParams params = holder.zoneImageView.getLayoutParams();
+
+                params.width = holder.zoneImageView.getMeasuredWidth() - context.getResources().getInteger(R.integer.zone_grid_layout_param);
+                params.height = holder.zoneImageView.getMeasuredWidth() - context.getResources().getInteger(R.integer.zone_grid_layout_param);
+                holder.zoneImageView.setLayoutParams(params);
             }
         });
     }
@@ -54,6 +70,8 @@ public class ZoneAdapter extends RecyclerView.Adapter<ZoneAdapter.ZoneViewHolder
         ImageView zoneImageView;
         @BindView(R.id.zone_title_text_view)
         TextView zoneTitle;
+        @BindView(R.id.followers_count_layout)
+        LinearLayout followersCount;
         @BindView(R.id.follow_image_view)
         ImageView followTick;
 
