e36d5eab915fa7aa370d3ef1793e3f21816e62dd
plank-dhamini <dhamini@plank.life>
2018-07-23 15:54:04 +0530

    JA-345 #comment Sign in view * Refactored sign in view * Added stacked sign up card view in login view * Turned hardware accelerated to true in manifest file * TODO: navigate to sign up screen on clicking sign Up card

--- app/src/main/java/life/plank/juna/zone/view/activity/SignInActivity.java
+++ app/src/main/java/life/plank/juna/zone/view/activity/SignInActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
@@ -81,6 +82,7 @@ public class SignInActivity extends AppCompatActivity {
 
         emailEditText.addTextChangedListener(loginFieldsWatcher);
         passwordEditText.addTextChangedListener(loginFieldsWatcher);
+
     }
 
     private void initStackAnimation() {
