d8a89e49707ebe664d4b96e8314cc65b11af98df
PLANK\plank-prem <prem@plank.life>
2018-08-23 15:54:13 +0530

    JA-405 #comment
    * Converted GraphFragment from a Fragment to a View

--- app/src/main/java/life/plank/juna/zone/view/fragment/GraphFragment.java
+++ app/src/main/java/life/plank/juna/zone/view/fragment/GraphFragment.java
@@ -1,13 +1,12 @@
 package life.plank.juna.zone.view.fragment;
 
-import android.content.Context;
+import android.app.Fragment;
 import android.graphics.Color;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.util.AttributeSet;
-import android.util.SparseArray;
+import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.FrameLayout;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 
 import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.components.Legend;
@@ -19,50 +18,36 @@ import com.github.mikephil.charting.data.LineDataSet;
 import com.github.mikephil.charting.utils.ColorTemplate;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.concurrent.TimeUnit;
 
-import butterknife.BindView;
-import butterknife.ButterKnife;
 import life.plank.juna.zone.R;
 import life.plank.juna.zone.data.network.model.ScrubberViewData;
 import life.plank.juna.zone.interfaces.UpdateDataOnChartFragment;
 import life.plank.juna.zone.util.ScrubberConstants;
 
-public class GraphFragment extends FrameLayout implements UpdateDataOnChartFragment {
-
-    @BindView(R.id.line_chart)
+public class GraphFragment extends Fragment implements UpdateDataOnChartFragment {
+    RelativeLayout linearLayout;
     LineChart lineChart;
-
     ArrayList<Entry> values = new ArrayList<>();
     long now;
-    private SparseArray<ScrubberViewData> graphData;
+    private HashMap<Integer, ScrubberViewData> graphData;
     UpdateDataOnChartFragment updateDataOnChartFragment;
 
-    public GraphFragment(@NonNull Context context) {
-        this(context, null);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.graph_fragment, container, false);
+        linearLayout = view.findViewById(R.id.root);
+        lineChart = view.findViewById(R.id.line_chart);
+        setUpGraph();
+        return view;
     }
 
-    public GraphFragment(@NonNull Context context, @Nullable AttributeSet attrs) {
-        this(context, attrs, 0);
-    }
-
-    public GraphFragment(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
-        this(context, attrs, defStyleAttr, 0);
-    }
-
-    public GraphFragment(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
-        super(context, attrs, defStyleAttr, defStyleRes);
-        init(context);
-    }
-
-    public void init(Context context) {
-        View view = inflate(context, R.layout.graph_fragment, this);
-        ButterKnife.bind(this, view);
+    private void setUpGraph() {
         setUpChart();
     }
 
     private void setUpChart() {
-        graphData = new SparseArray<>();
+        graphData = new HashMap<>();
         ScrubberConstants.getHighLightsMatchOne(graphData);
         // no description text
         lineChart.getDescription().setEnabled(false);
@@ -111,8 +96,8 @@ public class GraphFragment extends FrameLayout implements UpdateDataOnChartFragm
         lineChart.invalidate();
     }
 
-    protected float getRandom(float range) {
-        return (float) (Math.random() * range) + (float) 0;
+    protected float getRandom(float range, float startsfrom) {
+        return (float) (Math.random() * range) + startsfrom;
     }
 
     private void setData(int count, float range) {
@@ -123,7 +108,7 @@ public class GraphFragment extends FrameLayout implements UpdateDataOnChartFragm
         float to = now + count;
         // increment by 1 hour
         for (float chartX = from; chartX < to; chartX++) {
-            float chartY = getRandom(range);
+            float chartY = getRandom(range, 0);
             values.add(new Entry(chartX, chartY)); // add one entry per hour
             LineDataSet set1 = new LineDataSet(values, "DataSet 1");
             set1.setAxisDependency(YAxis.AxisDependency.LEFT);
@@ -147,7 +132,6 @@ public class GraphFragment extends FrameLayout implements UpdateDataOnChartFragm
             lineChart.invalidate();
         }
     }
-
     @Override
     public void updateData(String id, int minute) {
         //todo: display data on graph here
