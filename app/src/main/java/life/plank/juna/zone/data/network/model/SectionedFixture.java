51cebd74145f6648c96fbfea353b405c03b8a706
PLANK\plank-prem <prem@plank.life>
2018-08-25 18:22:58 +0530

    Revert "JA-391 #comment"
    This reverts commit afa029380e215e602ea6d4e4d4976f7adde21a33.

---
+++ app/src/main/java/life/plank/juna/zone/data/network/model/SectionedFixture.java
@@ -0,0 +1,44 @@
+package life.plank.juna.zone.data.network.model;
+
+import java.util.List;
+
+import life.plank.juna.zone.domain.service.FootballFixtureClassifierService.FixtureSection;
+import lombok.Data;
+
+@Data
+public class SectionedFixture {
+    FixtureSection section;
+    String matchday;
+    String date;
+    List<ScoreFixture> scoreFixtureList;
+
+    private SectionedFixture(FixtureSection section, List<ScoreFixture> scoreFixtureList) {
+        this.section = section;
+        this.scoreFixtureList = scoreFixtureList;
+    }
+
+    private SectionedFixture(String matchday, FixtureSection section, List<ScoreFixture> scoreFixtureList) {
+        this.matchday = matchday;
+        this.section = section;
+        this.scoreFixtureList = scoreFixtureList;
+    }
+
+    private SectionedFixture(String matchday, String date, FixtureSection section, List<ScoreFixture> scoreFixtureList) {
+        this.matchday = matchday;
+        this.date = date;
+        this.section = section;
+        this.scoreFixtureList = scoreFixtureList;
+    }
+
+    public static SectionedFixture getFrom(FixtureSection classification, List<ScoreFixture> scoreFixtureList) {
+        return new SectionedFixture(classification, scoreFixtureList);
+    }
+
+    public static SectionedFixture getFrom(String matchday, FixtureSection section, List<ScoreFixture> scoreFixtureList) {
+        return new SectionedFixture(matchday, section, scoreFixtureList);
+    }
+
+    public static SectionedFixture getFrom(String matchday, String date, FixtureSection section, List<ScoreFixture> scoreFixtureList) {
+        return new SectionedFixture(matchday, date, section, scoreFixtureList);
+    }
+}
