d05d84d5abb10ac164b05e6000cb94da8e3642b7
plank-dhamini <dhamini@plank.life>
2018-08-30 12:06:13 +0530

    JA-396 #comment * Added bottom sheet to search user * Displaying the bottom sheet on clicking the first option in boom menu

--- app/src/main/java/life/plank/juna/zone/view/activity/SwipePageActivity.java
+++ app/src/main/java/life/plank/juna/zone/view/activity/SwipePageActivity.java
@@ -3,6 +3,8 @@ package life.plank.juna.zone.view.activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.support.design.widget.BottomSheetBehavior;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
@@ -10,6 +12,7 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
+import android.widget.SearchView;
 import android.widget.Toast;
 
 import com.bvapp.arcmenulibrary.ArcMenu;
@@ -52,6 +55,12 @@ public class SwipePageActivity extends AppCompatActivity implements PinFeedListe
     ProgressBar progressBar;
     @BindView(R.id.arc_menu)
     ArcMenu arcMenu;
+    @BindView(R.id.search_view)
+    SearchView search;
+    BottomSheetBehavior bottomSheetBehavior;
+    RecyclerView recyclerView;
+    CoordinatorLayout coordinatorLayout;
+
     FootballFeedAdapter adapter;
 
     private RecyclerView.OnScrollListener recyclerViewOnScrollListener = new RecyclerView.OnScrollListener() {
@@ -75,9 +84,18 @@ public class SwipePageActivity extends AppCompatActivity implements PinFeedListe
         ButterKnife.bind(this);
         swipePageActivity = SwipePageActivity.this;
         ((ZoneApplication) getApplication()).getUiComponent().inject(this);
+        recyclerView = findViewById(R.id.recycler_view);
+
         initRecyclerView();
         setUpData();
         setUpBoomMenu();
+
+        coordinatorLayout = findViewById(R.id.coordinator_layout);
+
+        View bottomSheet = findViewById(R.id.bottom_sheet);
+        bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet);
+        bottomSheetBehavior.setPeekHeight(0);
+
     }
 
     private void setUpData() {
@@ -153,6 +171,9 @@ public class SwipePageActivity extends AppCompatActivity implements PinFeedListe
                 public void onClick(View v) {
                     switch (position) {
                         case 0: {
+                            bottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+                            search.setQueryHint(getString(R.string.search_query_hint));
+
                             break;
                         }
                         case 1: {
