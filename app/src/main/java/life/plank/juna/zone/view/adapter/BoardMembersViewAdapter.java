b6503917c47176fdaf93ab196372d03faf4572bb
Arfaa Nisar Shariff <arfaa@plank.life>
2018-09-14 12:18:32 +0530

    JA-480 #comment * Added blurred background to InviteBoardActivity

--- app/src/main/java/life/plank/juna/zone/view/adapter/BoardMembersViewAdapter.java
+++ app/src/main/java/life/plank/juna/zone/view/adapter/BoardMembersViewAdapter.java
@@ -2,6 +2,7 @@ package life.plank.juna.zone.view.adapter;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -16,17 +17,23 @@ import butterknife.ButterKnife;
 import life.plank.juna.zone.R;
 import life.plank.juna.zone.data.network.model.User;
 import life.plank.juna.zone.view.activity.InviteToBoardActivity;
+import life.plank.juna.zone.view.fragment.board.user.PrivateBoardInfoFragment;
+
+import static life.plank.juna.zone.util.UIDisplayUtil.loadBitmap;
 
 public class BoardMembersViewAdapter extends RecyclerView.Adapter<BoardMembersViewAdapter.BoardMembersViewHolder> {
 
+    public static Bitmap parentViewBitmap = null;
     private List<User> userList;
     private Context context;
     private String boardId;
+    private PrivateBoardInfoFragment fragment;
 
-    public BoardMembersViewAdapter(List<User> userList, Context context, String boardId) {
+    public BoardMembersViewAdapter(List<User> userList, Context context, String boardId, PrivateBoardInfoFragment fragment) {
         this.context = context;
         this.userList = userList;
         this.boardId = boardId;
+        this.fragment = fragment;
     }
 
     @Override
@@ -42,6 +49,7 @@ public class BoardMembersViewAdapter extends RecyclerView.Adapter<BoardMembersVi
         }
 
         holder.profileImageView.setOnClickListener(view -> {
+            parentViewBitmap = loadBitmap(fragment.getActivity().getWindow().getDecorView(), fragment.getActivity().getWindow().getDecorView(), context);
             Intent inviteToBoard = new Intent(context, InviteToBoardActivity.class);
             inviteToBoard.putExtra(context.getString(R.string.intent_board_id), boardId);
             inviteToBoard.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
