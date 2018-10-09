ec96ab4026e321447461699932881d892c38acee
Arfaa Nisar Shariff <arfaa@plank.life>
2018-10-09 14:44:33 +0530

    JA-597 #comment * Displaying dummy notifications when the user navigates to the notification page * Added string to xml

--- app/src/main/java/life/plank/juna/zone/view/activity/UserNotificationActivity.java
+++ app/src/main/java/life/plank/juna/zone/view/activity/UserNotificationActivity.java
@@ -5,12 +5,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import life.plank.juna.zone.R;
 import life.plank.juna.zone.ZoneApplication;
 import life.plank.juna.zone.util.customview.ZoneToolBar;
+import life.plank.juna.zone.view.adapter.NotificationAdapter;
 
 
 public class UserNotificationActivity extends AppCompatActivity {
@@ -18,9 +23,23 @@ public class UserNotificationActivity extends AppCompatActivity {
 
     @BindView(R.id.toolbar)
     ZoneToolBar toolbar;
+    @BindView(R.id.notification_recycler_view)
+    RecyclerView notificationRecyclerView;
+    NotificationAdapter notificationAdapter;
+    private ArrayList<String> notificationArray = new ArrayList<>();
+
 
     public static void launch(Context packageContext) {
         packageContext.startActivity(new Intent(packageContext, UserNotificationActivity.class));
+    }
+
+    //TODO: Remove after integrating with the backend
+    private void populateNotificationArray() {
+        notificationArray.add("PikaPerfect has accepted your invite.");
+        notificationArray.add("AnnaBeautiful and 36 others has replied to your comment.");
+        notificationArray.add("CoolRahlf and 5 others has liked your post.");
+        notificationArray.add("PrettyPopo has replied to your comment");
+        notificationArray.add("Xuan Xii  Cheng and 12 others has liked your comment.");
     }
 
     @Override
@@ -33,6 +52,14 @@ public class UserNotificationActivity extends AppCompatActivity {
         ((ZoneApplication) getApplicationContext()).getUiComponent().inject(this);
 
         toolbar.setTitle(getString(R.string.notification));
+        populateNotificationArray();
+        initRecyclerView();
     }
 
+    private void initRecyclerView() {
+        notificationAdapter = new NotificationAdapter(this, notificationArray);
+        notificationRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        notificationRecyclerView.setAdapter(notificationAdapter);
+
+    }
 }
---
+++ app/src/main/java/life/plank/juna/zone/view/adapter/NotificationAdapter.java
@@ -0,0 +1,59 @@
+package life.plank.juna.zone.view.adapter;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import life.plank.juna.zone.R;
+
+public class NotificationAdapter extends RecyclerView.Adapter<NotificationAdapter.NotificationViewHolder> {
+
+    private static final String TAG = NotificationAdapter.class.getSimpleName();
+    private Context context;
+    private ArrayList<String> notificationList;
+
+
+    public NotificationAdapter(Context context, ArrayList<String> notificationList) {
+        this.context = context;
+        this.notificationList = notificationList;
+    }
+
+    @Override
+    public NotificationViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return new NotificationViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.item_notification, parent, false));
+
+    }
+
+    @Override
+    public void onBindViewHolder(NotificationViewHolder holder, int position) {
+        holder.notification.setText(notificationList.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return notificationList.size();
+    }
+
+    public class NotificationViewHolder extends RecyclerView.ViewHolder {
+
+        @BindView(R.id.profile_pic)
+        ImageView profileImage;
+        @BindView(R.id.notification)
+        TextView notification;
+        @BindView(R.id.time)
+        TextView time;
+
+        NotificationViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+        }
+    }
+}
