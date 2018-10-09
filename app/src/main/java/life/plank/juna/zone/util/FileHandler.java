0353db0348d9f47f93b95acbaa6123ff3d22fc50
Prem Suman <prem@plank.life>
2018-10-09 09:48:50 +0530

    JA-594 #comment
    * Added Javadoc for FileHandler class

--- app/src/main/java/life/plank/juna/zone/util/FileHandler.java
+++ app/src/main/java/life/plank/juna/zone/util/FileHandler.java
@@ -12,6 +12,9 @@ import java.io.FileOutputStream;
 import static life.plank.juna.zone.ZoneApplication.getContext;
 import static life.plank.juna.zone.util.UIDisplayUtil.getScreenshot;
 
+/**
+ * Class for handling file management, i.e., saving and retrieving a file/image from the app's private storage
+ */
 public class FileHandler {
 
     public static void saveScreenshot(String activityTag, View screenshotView) {
