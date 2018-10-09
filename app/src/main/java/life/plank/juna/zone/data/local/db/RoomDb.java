3f2cef4ef7d3a4fb112f46166961387df7421e04
Prem Suman <prem@plank.life>
2018-10-09 12:12:32 +0530

    JA-599 #comment
    * Created singleton Room database class.

---
+++ app/src/main/java/life/plank/juna/zone/data/local/db/RoomDb.java
@@ -0,0 +1,38 @@
+package life.plank.juna.zone.data.local.db;
+
+import android.arch.persistence.room.Database;
+import android.arch.persistence.room.Room;
+import android.arch.persistence.room.RoomDatabase;
+
+import life.plank.juna.zone.ZoneApplication;
+
+@Database(entities = {/*TODO : enter entity classes here*/}, version = 1, exportSchema = false)
+public abstract class RoomDb extends RoomDatabase {
+
+    private static final String ROOM_DB_NAME = "junaDatabase.db";
+    private static RoomDb INSTANCE;
+
+    private RoomDb() {
+//        Reflection safety
+        if (INSTANCE != null) {
+            throw new RuntimeException("Use getInstance() method to get the single instance of this class");
+        }
+    }
+
+    public static RoomDb getInstance() {
+        if (INSTANCE == null) {
+//            synchronized block and a second null-check for thread safety
+            synchronized (RoomDb.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = Room.databaseBuilder(ZoneApplication.getContext(), RoomDb.class, ROOM_DB_NAME).build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
+
+    //    returning same instance when serializing/de-serializing
+    protected RoomDb readResolve() {
+        return getInstance();
+    }
+}
