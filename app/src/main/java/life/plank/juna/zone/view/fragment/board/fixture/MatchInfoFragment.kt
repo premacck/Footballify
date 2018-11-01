a4b8fe1d4ec7045b321b558b446d49b9dbf66c6b
plank-dhamini <dhamini@plank.life>
2018-11-01 12:42:19 +0530

    JA-660 #comment * Added code to load boardInfo fragment on clicking lineups tab * TODO: Remove color filter method, Replace boardInfo fragment with lineup fragment

--- app/src/main/java/life/plank/juna/zone/view/fragment/board/fixture/MatchInfoFragment.kt
+++ app/src/main/java/life/plank/juna/zone/view/fragment/board/fixture/MatchInfoFragment.kt
@@ -1,6 +1,8 @@
 package life.plank.juna.zone.view.fragment.board.fixture
 
 
+import android.content.*
+import android.graphics.PorterDuff
 import android.os.Bundle
 import android.support.design.widget.TabLayout
 import android.support.v4.app.Fragment
@@ -8,24 +10,52 @@ import android.support.v4.app.FragmentManager
 import android.support.v4.app.FragmentStatePagerAdapter
 import android.support.v4.view.PagerAdapter
 import android.support.v7.widget.CardView
+import android.util.Log
+import android.util.Pair
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import com.google.firebase.messaging.FirebaseMessaging
+import com.google.gson.Gson
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.fragment_match_board.*
 import kotlinx.android.synthetic.main.fragment_match_info.*
 import life.plank.juna.zone.R
 import life.plank.juna.zone.ZoneApplication
+import life.plank.juna.zone.data.RestApiAggregator
+import life.plank.juna.zone.data.model.Board
 import life.plank.juna.zone.data.model.League
+import life.plank.juna.zone.data.model.MatchDetails
 import life.plank.juna.zone.data.model.MatchFixture
+import life.plank.juna.zone.data.network.interfaces.RestApi
+import life.plank.juna.zone.util.AppConstants
 import life.plank.juna.zone.util.DataUtil
+import life.plank.juna.zone.util.FixtureListUpdateTask
+import life.plank.juna.zone.util.UIDisplayUtil
 import life.plank.juna.zone.util.facilis.BaseCard
+import rx.Subscriber
 import java.lang.ref.WeakReference
+import javax.inject.Inject
+import javax.inject.Named
 
 class MatchInfoFragment : BaseCard() {
 
+    @Inject
+    lateinit var gson: Gson
+    @field: [Inject Named("default")]
+    lateinit var restApi: RestApi
+    @field: [Inject Named("footballData")]
+    lateinit var footballRestApi: RestApi
+    @Inject
+    lateinit var picasso: Picasso
+    private var matchDetails: MatchDetails? = null
+    private var boardId: String? = null
     private var currentMatchId: Long = 0
     private lateinit var league: League
     private var fixture: MatchFixture? = null
     private var infoPagerAdapter: MatchInfoFragment.InfoPagerAdapter? = null
+    private var isBoardActive: Boolean = false
 
     override fun getBackgroundBlurLayout(): ViewGroup? = null
 
@@ -39,6 +69,14 @@ class MatchInfoFragment : BaseCard() {
             arguments = Bundle().apply {
                 putParcelable(DataUtil.findString(R.string.intent_fixture_data), fixture)
                 putParcelable(DataUtil.findString(R.string.intent_league), league)
+            }
+        }
+    }
+
+    private val mMessageReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context, intent: Intent) {
+            if (intent.hasExtra(getString(R.string.intent_zone_live_data))) {
+                setZoneLiveData(intent)
             }
         }
     }
@@ -67,7 +105,7 @@ class MatchInfoFragment : BaseCard() {
         super.onViewCreated(view, savedInstanceState)
         home_team.text = fixture?.homeTeam?.name
         visiting_team.text = fixture?.awayTeam?.name
-        setupViewPagerWithFragments()
+        getBoardIdAndMatchDetails(currentMatchId)
     }
 
     private fun setupViewPagerWithFragments() {
@@ -75,6 +113,109 @@ class MatchInfoFragment : BaseCard() {
         info_view_pager.adapter = infoPagerAdapter
         info_view_pager.addOnPageChangeListener(TabLayout.TabLayoutOnPageChangeListener(info_tiles_tab_layout))
         info_tiles_tab_layout.addOnTabSelectedListener(TabLayout.ViewPagerOnTabSelectedListener(info_view_pager))
+    }
+
+    override fun onResume() {
+        super.onResume()
+        context?.registerReceiver(mMessageReceiver, IntentFilter(getString(R.string.intent_board)))
+    }
+
+    override fun onPause() {
+        super.onPause()
+        context?.unregisterReceiver(mMessageReceiver)
+    }
+
+    private fun setZoneLiveData(intent: Intent) {
+        val zoneLiveData = DataUtil.getZoneLiveData(intent, getString(R.string.intent_zone_live_data), gson)
+        when (zoneLiveData!!.liveDataType) {
+            AppConstants.SCORE_DATA -> {
+                val scoreData = zoneLiveData.getScoreData(gson)
+                DataUtil.updateScoreLocally(fixture!!, scoreData)
+                DataUtil.updateScoreLocally(matchDetails!!, scoreData)
+
+                FixtureListUpdateTask.update(fixture, scoreData, null, true)
+            }
+            AppConstants.TIME_STATUS_DATA -> {
+                val timeStatus = zoneLiveData.getLiveTimeStatus(gson)
+                DataUtil.updateTimeStatusLocally(fixture!!, timeStatus)
+                DataUtil.updateTimeStatusLocally(matchDetails!!, timeStatus)
+                FixtureListUpdateTask.update(fixture, null, timeStatus, false)
+            }
+            AppConstants.BOARD_ACTIVATED -> {
+                isBoardActive = true
+                clearColorFilter()
+                setupViewPagerWithFragments()
+            }
+            AppConstants.BOARD_DEACTIVATED -> {
+                isBoardActive = false
+                applyInactiveBoardColorFilter()
+                UIDisplayUtil.showBoardExpirationDialog(activity) { dialog, _ ->
+                    setupViewPagerWithFragments()
+                    dialog.cancel()
+                }
+            }
+        }
+        try {
+            if (infoPagerAdapter?.currentFragment is BoardInfoFragment) {
+                (infoPagerAdapter?.currentFragment as? BoardInfoFragment)?.updateZoneLiveData(zoneLiveData)
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
+    private fun getBoardIdAndMatchDetails(currentMatchId: Long?) {
+        RestApiAggregator.getBoardAndMatchDetails(restApi, footballRestApi, currentMatchId!!)
+                .doOnSubscribe { info_progress_bar!!.visibility = View.VISIBLE }
+                .doOnTerminate { info_progress_bar!!.visibility = View.GONE }
+                .subscribe(object : Subscriber<Pair<Board, MatchDetails>>() {
+                    override fun onCompleted() {
+                        Log.i(MatchInfoFragment.TAG, "onCompleted: getBoardIdAndMatchDetails")
+                    }
+
+                    override fun onError(e: Throwable) {
+                        Log.e(MatchInfoFragment.TAG, "In onError() : getBoardIdAndMatchDetails$e")
+                        Toast.makeText(context, R.string.something_went_wrong, Toast.LENGTH_LONG).show()
+                    }
+
+                    override fun onNext(boardMatchDetailsPair: Pair<Board, MatchDetails>?) {
+                        if (boardMatchDetailsPair != null) {
+                            matchDetails = boardMatchDetailsPair.second
+                            val board = boardMatchDetailsPair.first
+                            if (matchDetails != null) {
+                                matchDetails!!.league = league
+                            }
+                            if (board != null) {
+                                boardId = board.id
+                                saveBoardId()
+                                isBoardActive = board.isActive!!
+
+                                if (isBoardActive) {
+                                    FirebaseMessaging.getInstance().subscribeToTopic(getString(R.string.board_id_prefix) + boardId!!)
+                                    FirebaseMessaging.getInstance().subscribeToTopic(getString(R.string.pref_football_match_sub) + currentMatchId)
+                                } else
+                                    applyInactiveBoardColorFilter()
+                            } else
+                                applyInactiveBoardColorFilter()
+                            setupViewPagerWithFragments()
+                        } else {
+                            Toast.makeText(context, R.string.something_went_wrong, Toast.LENGTH_LONG).show()
+                        }
+                    }
+                })
+    }
+
+    fun saveBoardId() {
+        val boardIdEditor: SharedPreferences.Editor = activity?.getSharedPreferences(getString(R.string.pref_enter_board_id), Context.MODE_PRIVATE)!!.edit()
+        boardIdEditor.putString(getString(R.string.pref_enter_board_id), boardId).apply()
+    }
+
+    private fun clearColorFilter() {
+        board_parent_layout!!.background.clearColorFilter()
+    }
+
+    private fun applyInactiveBoardColorFilter() {
+        board_parent_layout!!.background.setColorFilter(UIDisplayUtil.findColor(R.color.grey_0_7), PorterDuff.Mode.SRC_OVER)
     }
 
     class InfoPagerAdapter(supportFragmentManager: FragmentManager, matchInfoFragment: MatchInfoFragment) : FragmentStatePagerAdapter(supportFragmentManager) {
@@ -87,7 +228,7 @@ class MatchInfoFragment : BaseCard() {
                 return when (position) {
                     //TODO: Replace dummy fragment with required fragment
                     0 -> PrematchInfoFragment()
-                    1 -> DummyFragment()
+                    1 -> BoardInfoFragment.newInstance(gson.toJson(matchDetails))
                     2 -> DummyFragment()
                     3 -> DummyFragment()
                     4 -> DummyFragment()
