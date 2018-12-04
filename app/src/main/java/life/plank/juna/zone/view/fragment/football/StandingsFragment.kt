8a75f0a8a892c04278929538d877c8d49b6da223
Prem Suman <prem@plank.life>
2018-12-04 13:22:44 +0530

    JA-723
    * Improved card enter and exit animations
    
    Signed-off-by: Prem Suman <prem@plank.life>

--- app/src/main/java/life/plank/juna/zone/view/fragment/football/StandingsFragment.kt
+++ app/src/main/java/life/plank/juna/zone/view/fragment/football/StandingsFragment.kt
@@ -18,6 +18,7 @@ import life.plank.juna.zone.util.common.DataUtil
 import life.plank.juna.zone.util.common.DataUtil.isNullOrEmpty
 import life.plank.juna.zone.util.common.onTerminate
 import life.plank.juna.zone.util.common.setObserverThreadsAndSmartSubscribe
+import life.plank.juna.zone.util.facilis.doAfterDelay
 import life.plank.juna.zone.view.adapter.league.StandingTableAdapter
 import life.plank.juna.zone.view.fragment.base.BaseLeagueFragment
 import javax.inject.Inject
@@ -46,12 +47,14 @@ class StandingsFragment : BaseLeagueFragment() {
             inflater.inflate(R.layout.fragment_standings, container, false)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        standingTableAdapter = StandingTableAdapter(Glide.with(this), false)
-        standing_recycler_view.adapter = standingTableAdapter
+        context?.doAfterDelay(300) {
+            standingTableAdapter = StandingTableAdapter(Glide.with(this), false)
+            standing_recycler_view.adapter = standingTableAdapter
 
-        getStandings(false)
-        standings_swipe_refresh_layout.setOnRefreshListener { getStandings(true) }
-        standing_recycler_view.setupWithParentFragmentBoomMenu()
+            getStandings(false)
+            standings_swipe_refresh_layout.setOnRefreshListener { getStandings(true) }
+            standing_recycler_view.setupWithParentFragmentBoomMenu()
+        }
     }
 
     override fun restApi(): RestApi = restApi
