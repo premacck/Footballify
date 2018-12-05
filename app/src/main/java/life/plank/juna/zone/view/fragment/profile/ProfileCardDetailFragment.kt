f3e8fd61e61e9590102ab0859dcc35544b8f1186
Prem Suman <prem@plank.life>
2018-12-05 17:03:56 +0530

    JA-729
    * Updated kotlin file ProfileCardDetailFragment with new overridden methods from BaseCard
    
    Signed-off-by: Prem Suman <prem@plank.life>

--- app/src/main/java/life/plank/juna/zone/view/fragment/profile/ProfileCardDetailFragment.kt
+++ app/src/main/java/life/plank/juna/zone/view/fragment/profile/ProfileCardDetailFragment.kt
@@ -1,7 +1,6 @@
 package life.plank.juna.zone.view.fragment.profile
 
 import android.os.Bundle
-import android.support.v7.widget.CardView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -34,8 +33,7 @@ class ProfileCardDetailFragment : BaseCard() {
 
     override fun getBackgroundBlurLayout(): BlurLayout? = blur_layout
 
-    override fun getRootCard(): CardView? = root_card
+    override fun getRootView(): ViewGroup? = root_card
 
-    override fun getDragHandle(): View? = drag_area
-
+    override fun getDragView(): View? = drag_area
 }
