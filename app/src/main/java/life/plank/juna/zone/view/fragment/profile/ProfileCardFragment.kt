4dfba3da27e7c79812a6fc0c8caa0bcc1647087e
Arfaa Nisar Shariff <arfaa@plank.life>
2018-12-05 17:49:05 +0530

    JA-728 #comment * Fixed layout issue where blur background was not visible

--- app/src/main/java/life/plank/juna/zone/view/fragment/profile/ProfileCardFragment.kt
+++ app/src/main/java/life/plank/juna/zone/view/fragment/profile/ProfileCardFragment.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.RelativeLayout
 import io.alterac.blurkit.BlurLayout
 import kotlinx.android.synthetic.main.user_profile_card.*
 import life.plank.juna.zone.R
@@ -22,7 +23,7 @@ class ProfileCardFragment : BaseCard() {
 
     override fun getBackgroundBlurLayout(): BlurLayout? = blur_layout
 
-    override fun getRootView(): ViewGroup? = root_card
+    override fun getRootView(): RelativeLayout? = root_card
 
     override fun getDragView(): View? = drag_area
 
