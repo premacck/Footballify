d9401be7ee9df94d22668e090c3225a4f85086d8
Prem Suman <prem@plank.life>
2018-12-07 17:08:07 +0530

    JA-729
    * Code cleanup
    
    Signed-off-by: Prem Suman <prem@plank.life>

--- app/src/main/java/life/plank/juna/zone/view/latestMatch/LeagueBinder.kt
+++ app/src/main/java/life/plank/juna/zone/view/latestMatch/LeagueBinder.kt
@@ -19,7 +19,6 @@ class LeagueBinder(private val onItemClickListener: OnItemClickListener, private
     override fun create(inflater: LayoutInflater, parent: ViewGroup): LeagueViewHolder = LeagueViewHolder(inflater.inflate(R.layout.item_league, parent, false))
 
     override fun bind(holder: LeagueViewHolder, item: LeagueModel) {
-        // Bind the data here
         holder.itemView.all_leagues_card.setOnClickListener {
             onItemClickListener.onItemClicked()
         }
