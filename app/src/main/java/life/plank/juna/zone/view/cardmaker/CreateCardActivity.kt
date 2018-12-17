5fd1711a98f06ad316dbc0fba59be9103d476d43
plank-dhamini <dhamini@plank.life>
2018-12-17 12:49:07 +0530

    JA-742 #comment * Select image from camera / gallery to create a card * If the boardId is null then navigate to card creator else navigate to uploadActivity

--- app/src/main/java/life/plank/juna/zone/view/cardmaker/CreateCardActivity.kt
+++ app/src/main/java/life/plank/juna/zone/view/cardmaker/CreateCardActivity.kt
@@ -1,22 +1,34 @@
 package life.plank.juna.zone.view.cardmaker
 
 import android.app.Activity
+import android.content.Intent
+import android.graphics.BitmapFactory
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_create_card.*
 import life.plank.juna.zone.R
 import life.plank.juna.zone.ZoneApplication
 import life.plank.juna.zone.data.network.interfaces.RestApi
 import life.plank.juna.zone.view.activity.base.BaseCardActivity
-import org.jetbrains.anko.intentFor
+import life.plank.juna.zone.view.activity.camera.CustomCameraActivity
+import org.jetbrains.anko.sdk27.coroutines.onClick
 import javax.inject.Inject
+
 
 class CreateCardActivity : BaseCardActivity() {
 
     @Inject
     lateinit var restApi: RestApi
+    private var filePath: String? = null
 
     companion object {
         private val TAG = CreateCardActivity::class.java.simpleName
-        fun launch(from: Activity) = from.startActivity(from.intentFor<CreateCardActivity>())
+        fun launch(from: Activity, mediaFilePath: String) {
+            val intent = Intent(from, CreateCardActivity::class.java)
+            if (mediaFilePath != null && mediaFilePath.isNotEmpty()) {
+                intent.putExtra(from.getString(R.string.intent_file_path), mediaFilePath)
+            }
+            from.startActivity(intent)
+        }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,6 +36,18 @@ class CreateCardActivity : BaseCardActivity() {
         setContentView(R.layout.activity_create_card)
         ZoneApplication.getApplication().uiComponent.inject(this)
 
+        if (intent.hasExtra(getString(R.string.intent_file_path))) {
+            filePath = intent.getStringExtra(getString(R.string.intent_file_path))
+        }
+
+        if (!filePath.isNullOrEmpty()) {
+            val myBitmap = BitmapFactory.decodeFile(filePath)
+            profile_image_view.setImageBitmap(myBitmap)
+        }
+        camera.onClick {
+            CustomCameraActivity.launch(this@CreateCardActivity, true, "")
+            finish()
+        }
     }
 
     override fun getFragmentContainer(): Int = R.id.popup_container
