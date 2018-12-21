900e3beaf709ee9e0888507c9aff584d4827a196
Prem Suman <prem@plank.life>
2018-12-21 18:16:58 +0530

    JA-635
    * UI improvements in notification list
    
    Signed-off-by: Prem Suman <prem@plank.life>

--- app/src/main/java/life/plank/juna/zone/view/adapter/common/NotificationAdapter.kt
+++ app/src/main/java/life/plank/juna/zone/view/adapter/common/NotificationAdapter.kt
@@ -1,15 +1,19 @@
 package life.plank.juna.zone.view.adapter.common
 
+import android.text.*
 import android.view.*
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import kotlinx.android.synthetic.main.item_notification.view.*
 import life.plank.juna.zone.R
 import life.plank.juna.zone.data.model.notification.SocialNotification
 import life.plank.juna.zone.notification.*
-import life.plank.juna.zone.util.common.execute
+import life.plank.juna.zone.util.common.*
 import life.plank.juna.zone.util.facilis.onFancyClick
 import life.plank.juna.zone.util.sharedpreference.PreferenceManager.Auth.getToken
 import life.plank.juna.zone.util.time.getTimeAgo
+import life.plank.juna.zone.util.view.UIDisplayUtil.getDp
 import life.plank.juna.zone.view.activity.base.BaseCardActivity
 
 class NotificationAdapter(private val activity: BaseCardActivity) : RecyclerView.Adapter<NotificationAdapter.NotificationViewHolder>() {
@@ -22,14 +26,29 @@ class NotificationAdapter(private val activity: BaseCardActivity) : RecyclerView
     override fun onBindViewHolder(holder: NotificationViewHolder, position: Int) {
         notificationList[position].run {
             holder.itemView.run {
-                notification_message.text = notificationMessage
+                notification_message.text = notificationMessage.formatAsNotificationMessage(userHandles)
                 notification_time.text = getTimeAgo(date)
+                Glide.with(activity)
+                        .load(iconUrl?.get(0))
+                        .apply(RequestOptions.circleCropTransform().override(getDp(20f).toInt(), getDp(20f).toInt()))
+                        .into(profile_pic)
             }
             holder.itemView.onFancyClick {
                 activity.restApi()?.setNotificationAsRead(id, getToken())?.execute()
                 activity.triggerNotificationIntent(getSocialNotificationIntent())
             }
         }
+    }
+
+    private fun String.formatAsNotificationMessage(userHandles: List<String>): SpannableStringBuilder {
+        val message = SpannableString(this)
+        userHandles.forEach {
+            if (contains(it, true)) {
+                val startIndex = indexOf(it, ignoreCase = true)
+                message.bold(startIndex, startIndex + it.length)
+            }
+        }
+        return SpannableStringBuilder(message)
     }
 
     override fun getItemCount(): Int = notificationList.size
