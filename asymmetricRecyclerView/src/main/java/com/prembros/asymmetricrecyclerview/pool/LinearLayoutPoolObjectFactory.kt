9072cd972a5b07703e231389dc57ff22ba7cedd4
Prem Ankur <premacck1@ufl.edu>
2019-02-01 14:49:22 +0530

    Delete LinearLayoutPoolObjectFactory.kt

--- asymmetricRecyclerView/src/main/java/com/prembros/asymmetricrecyclerview/pool/LinearLayoutPoolObjectFactory.kt
+++
@@ -1,8 +0,0 @@
-package com.prembros.asymmetricrecyclerview.pool
-
-import android.content.Context
-import android.widget.LinearLayout
-
-class LinearLayoutPoolObjectFactory(private val context: Context) : PoolObjectFactory<LinearLayout> {
-    override fun createObject(): LinearLayout = LinearLayout(context, null)
-}
