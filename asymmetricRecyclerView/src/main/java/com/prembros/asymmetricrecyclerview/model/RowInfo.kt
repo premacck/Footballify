897d586a07341462534dd80310963ac08c162863
Prem Ankur <premacck1@ufl.edu>
2019-02-01 14:49:07 +0530

    Delete RowInfo.kt

--- asymmetricRecyclerView/src/main/java/com/prembros/asymmetricrecyclerview/model/RowInfo.kt
+++
@@ -1,7 +0,0 @@
-package com.prembros.asymmetricrecyclerview.model
-
-import android.os.Parcelable
-import kotlinx.android.parcel.*
-
-@Parcelize
-data class RowInfo(val rowHeight: Int, val items: @RawValue MutableList<RowItem>, val spaceLeft: Float) : Parcelable
