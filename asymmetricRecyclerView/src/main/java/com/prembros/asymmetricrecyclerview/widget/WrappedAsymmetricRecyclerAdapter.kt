2457fd9bc88df733d3ee885ef97c1168aa2d5898
Prem Ankur <premacck1@ufl.edu>
2019-02-01 14:47:29 +0530

    Delete WrappedAsymmetricRecyclerAdapter.kt

--- asymmetricRecyclerView/src/main/java/com/prembros/asymmetricrecyclerview/widget/WrappedAsymmetricRecyclerAdapter.kt
+++
@@ -1,8 +0,0 @@
-package com.prembros.asymmetricrecyclerview.widget
-
-import androidx.recyclerview.widget.RecyclerView
-import com.prembros.asymmetricrecyclerview.base.AsymmetricItem
-
-abstract class WrappedAsymmetricRecyclerAdapter<VH : RecyclerView.ViewHolder> : RecyclerView.Adapter<VH>() {
-    abstract fun getItem(position: Int): AsymmetricItem
-}
